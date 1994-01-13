a14bf060d0f3dbd299df237199a27b18cc518183
Michael I. Bushnell <mib@gnu.org>
1994-01-13 21:13:20 +0000

    Initial revision

---
+++ libpager/no-senders.c
@@ -0,0 +1,52 @@
+/* Called when a nosenders notification happens
+   Copyright (C) 1994 Free Software Foundation
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+
+
+void
+pager_no_senders (struct pager *p,
+		  mach_port_seqno_t seqno,
+		  mach_port_mscount_t mscount);
+{
+  mach_port_t old;
+  int ret;
+  
+  mutex_lock (&p->interlock);
+  _pager_wait_for_seqno (p, seqno);
+  _pager_release_seqno (p);
+  
+  if (mscount > p->mscount)
+    {
+      printf ("pager strange no senders\n");
+      done_with_port (p);
+    }
+  else if (mscount == p->mscount)
+    done_with_port (p);
+  else
+    {
+      /* Request a new notification.  The sync value is because we might
+	 have accounted for a new sender but not actually made the send right
+	 yet.  */
+      mach_port_request_notification (mach_task_self (), p->port.port, 
+				      MACH_NOTIFY_NO_SENDERS, p->mscount,
+				      p->port.port, 
+				      MACH_MSG_TYPE_MAKE_SEND_ONCE, &old);
+      if (old)
+	mach_port_deallocate (mach_task_self (), old);
+    }
+}
+
+
