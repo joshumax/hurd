e431e07f6201f691b47e93dacd73afa379b55ed9
Michael I. Bushnell <mib@gnu.org>
1994-01-19 19:40:55 +0000

    Formerly pager-create.c.~4~

--- libpager/pager-create.c
+++ libpager/pager-create.c
@@ -15,6 +15,8 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
 
+#include "priv.h"
+
 struct pager *
 pager_create (struct user_pager_info *upi)
 {
