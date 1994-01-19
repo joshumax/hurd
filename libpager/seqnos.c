b6dc0b29e0e7c01cb3058ac8dea6ec1c52574282
Michael I. Bushnell <mib@gnu.org>
1994-01-19 20:39:55 +0000

    Formerly seqnos.c.~2~

--- libpager/seqnos.c
+++ libpager/seqnos.c
@@ -15,6 +15,8 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
 
+#include "priv.h"
+
 /* Wait until all preceding messages on this port have
    had a chance to be honored. */
 void
