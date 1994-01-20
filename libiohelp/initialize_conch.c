4f66fdc81dd27032d4bdabefa4f7bb508e3a4422
Michael I. Bushnell <mib@gnu.org>
1994-01-20 21:34:43 +0000

    Formerly initialize_conch.c.~3~

--- libiohelp/initialize_conch.c
+++ libiohelp/initialize_conch.c
@@ -1,5 +1,5 @@
 /* 
-   Copyright (C) 1993 Free Software Foundation
+   Copyright (C) 1993, 1994 Free Software Foundation
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License as
@@ -15,7 +15,7 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
 
-#include <libioserver.h>
+#include "ioserver.h"
 
 void
 initialize_conch (struct conch *c, struct mutex *m)
