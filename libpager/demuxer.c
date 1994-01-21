7101f88955e788b816f982790cf6c6813e9682b0
Michael I. Bushnell <mib@gnu.org>
1994-01-21 23:36:13 +0000

    Formerly demuxer.c.~2~

--- libpager/demuxer.c
+++ libpager/demuxer.c
@@ -15,6 +15,8 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
 
+#include "priv.h"
+#include "memory_object.h"
 
 int
 pager_demuxer (mach_msg_header_t *inp,
