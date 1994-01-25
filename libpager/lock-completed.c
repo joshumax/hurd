8339cd1cd554f322d3c59af8a87869849cb7165a
Michael I. Bushnell <mib@gnu.org>
1994-01-25 21:06:48 +0000

    Formerly lock-completed.c.~4~

--- libpager/lock-completed.c
+++ libpager/lock-completed.c
@@ -19,8 +19,9 @@
 #include "memory_object.h"
 #include <stdio.h>
 
-/* A lock_request has finished.  Do our part of the wakeup
-   process.  */
+/* The kernel calls this (as described in <mach/memory_object.defs>)
+   when a memory_object_lock_request call has completed.  Read this
+   in combination with lock-object.c.  */
 error_t
 _pager_seqnos_memory_object_lock_completed (mach_port_t object,
 					    mach_port_seqno_t seqno,
