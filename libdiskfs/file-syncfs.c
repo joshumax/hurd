60204e1e42895b72f903677d6ae6db892e57195c
Michael I. Bushnell <mib@gnu.org>
1994-01-31 20:15:28 +0000

    Formerly file-syncfs.c.~2~

---
+++ libdiskfs/file-syncfs.c
@@ -0,0 +1,35 @@
+/* libdiskfs implementation of fs.defs: file_syncfs
+   Copyright (C) 1992, 1993, 1994 Free Software Foundation
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+
+#include "priv.h"
+
+/* Implement file_syncfs as described in <hurd/fs.defs>. */
+error_t
+diskfs_S_file_syncfs (struct protid *cred,
+		      int wait,
+		      int dochildren)
+{
+  if (!cred)
+    return EOPNOTSUPP;
+  
+  if (dochildren)
+    diskfs_sync_translators (wait);
+
+  diskfs_sync_everything (wait);
+  diskfs_save_hypermetadata (wait, 0);
+  return 0;
+}
