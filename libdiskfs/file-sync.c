4d6e6a88e436ed068b28f1de10ad3608b91c1d12
Michael I. Bushnell <mib@gnu.org>
1994-01-31 20:14:49 +0000

    Formerly file-sync.c.~2~

---
+++ libdiskfs/file-sync.c
@@ -0,0 +1,37 @@
+/* libdiskfs implementation of fs.defs: file_seek
+   Copyright (C) 1992, 1993, 1994 Free Software Foundation
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+
+#include "priv.h"
+
+/* Implement file_sync as described in <hurd/fs.defs>. */
+error_t
+diskfs_S_file_sync (struct protid *cred,
+		    int wait)
+{
+  struct node *np;
+
+  if (!cred)
+    return EOPNOTSUPP;
+  
+  np = cred->po->np;
+  
+  mutex_lock (&np->lock);
+  ioserver_get_conch (&np->conch);
+  mutex_unlock (&np->lock);
+  diskfs_file_update (np, wait);
+  return 0;
+}
