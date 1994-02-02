571195822a4d9f4b56ac175b194dc3de94437912
Michael I. Bushnell <mib@gnu.org>
1994-02-02 21:33:29 +0000

    Initial revision

---
+++ libdiskfs/peropen-make.c
@@ -0,0 +1,33 @@
+/* 
+   Copyright (C) 1994 Free Software Foundation
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+
+#include "priv.h"
+
+/* Create and return a new peropen structure on node NP with open
+   flags FLAGS.  */
+struct peropen *
+diskfs_make_peropen (struct node *np, int flags)
+{
+  struct peropen *po = malloc (sizeof (struct peropen));
+  diskfs_nref (np);
+  po->filepointer = 0;
+  po->flock_status = LOCK_UN;
+  po->refcnt = 0;
+  po->openstat = flags;
+  po->np = np;
+  return po;
+}
