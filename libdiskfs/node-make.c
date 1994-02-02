2ed64b6d3cf070701b6d3f2eab23162f6a665e32
Michael I. Bushnell <mib@gnu.org>
1994-02-02 21:11:50 +0000

    Formerly node-make.c.~2~

--- libdiskfs/node-make.c
+++ libdiskfs/node-make.c
@@ -15,4 +15,33 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
 
-diskfs_make_node
+#include "priv.h"
+
+
+/* Create a and return new node structure with DN as its physical disknode.
+   The node will have one user reference.  */
+struct node *
+diskfs_make_node (struct disknode *dn)
+{
+  struce node *np = malloc (sizeof (struct node));
+  
+  np->dn = dn;
+  np->dn_set_ctime = 0;
+  np->dn_set_atime = 0;
+  np->dn_set_mtime = 0;
+  
+  mutex_init (&np->lock);
+  np->references = 1;
+  np->owner = 0;
+  
+  fshelp_init_trans_link (&np->translator);
+  ioserver_initialize_conch (&np->conch, &np->lock);
+  
+  np->flock_type = LOCK_UN;
+  condition_init (&np->flockwait);
+  np->needflock = 0;
+  np->shlock_count = 0;
+  
+  diskfs_set_stat (np->dn, &np->dn_stat);
+  return np;
+}
