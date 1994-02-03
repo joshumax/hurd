b2f1ac17a7102fc4e31e19459d90664492fc9f7c
Michael I. Bushnell <mib@gnu.org>
1994-02-03 20:34:27 +0000

    Formerly io-modes-set.c.~2~

--- libdiskfs/io-modes-set.c
+++ libdiskfs/io-modes-set.c
@@ -15,6 +15,9 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
 
+#include "priv.h"
+#include "io_S.h"
+
 /* Implement io_set_all_openmodes as described in <hurd/io.defs>. */
 error_t
 S_io_set_all_openmodes (struct protid *cred,
@@ -24,7 +27,7 @@ S_io_set_all_openmodes (struct protid *cred,
     return EOPNOTSUPP;
   
   mutex_lock (&cred->po->ip->lock);
-  err = ioserver_get_conch (&np->conch);
+  ioserver_get_conch (&np->conch);
   if (!err)
     cred->po->openstat = (modes & HONORED_STATE_MODES);
   mutex_unlock (&cred->po->ip->lock);
