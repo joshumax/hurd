393eeae98b0249b2050e925a7a4afd5f861059ac
Michael I. Bushnell <mib@gnu.org>
1994-02-04 22:34:46 +0000

    Initial revision

---
+++ libdiskfs/node-times.c
@@ -0,0 +1,37 @@
+/*
+   Copyright (C) 1994 Free Software Foundation
+
+This file is part of the GNU Hurd.
+
+The GNU Hurd is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+The GNU Hurd is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with the GNU Hurd; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+/* Written by Michael I. Bushnell.  */
+
+/* If NP->dn_set_ctime is set, then modify NP->dn_stat.st_ctime
+   appropriately; do the analogous operation for atime and mtime as well.
+   Return nonzero iff any of the three was updated.  */
+int
+diskfs_set_node_times (struct node *np)
+{
+  int secs, usecs;
+  
+  do 
+    {
+      secs = _diskfs_mtime->seconds;
+      usecs = _diskfs_mtime->seconds;
+    }
+  while (secs != _diskfs_mtime->seconds);
+  
+  if (np->dn_set_mtime
