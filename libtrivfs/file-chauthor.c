baf29dc2a61ca8d1a1549f867d5bf265c60c7913
Michael I. Bushnell <mib@gnu.org>
1994-02-17 20:10:05 +0000

    Initial revision

---
+++ libtrivfs/file-chauthor.c
@@ -0,0 +1,27 @@
+/* 
+   Copyright (C) 1994 Free Software Foundation
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+
+#include "priv.h"
+
+error_t
+trivfs_file_chauthor (struct protid *cred,
+		      uid_t auth)
+{
+  spin_lock (&trivfs_nodelock);
+  if (trivfs_isowner (cred))
+    {
+      trivfs_stat.st_author = 
