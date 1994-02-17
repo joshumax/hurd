0c17c80eca45ff8248570a065274bbf5a7175bf0
Michael I. Bushnell <mib@gnu.org>
1994-02-17 23:34:37 +0000

    Initial revision

---
+++ libtrivfs/file-statfs.c
@@ -0,0 +1,28 @@
+/* 
+   Copyright (C) 1994 Free Software Foundation
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+
+#include "priv.h"
+
+error_t
+diskfs_S_file_statfs (struct protid *cred,
+		      struct fsys_statfsbuf *stb)
+{
+  bzero (stb, sizeof (struct fsys_statfsbuf));
+  stb->fsys_stb_type = trivfs_fstype;
+  stb->fsys_stb_fsid = trivfs_fsid;
+  return 0;
+}    
