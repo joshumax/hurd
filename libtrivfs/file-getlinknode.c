376e59dc3b25ebc6f44d44ae323bbd66b3c4fc25
Michael I. Bushnell <mib@gnu.org>
1994-02-17 22:38:04 +0000

    Initial revision

---
+++ libtrivfs/file-getlinknode.c
@@ -0,0 +1,30 @@
+/* 
+   Copyright (C) 1994 Free Software Foundation
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+
+#include "priv.h"
+
+error_t
+trivfs_S_file_getlinknode (struct protid *cred,
+			   mach_port_t *linknode,
+			   mach_msg_type_name_t *linknodetype)
+{
+  if (!cred)
+    return EOPNOTSUPP;
+  *linknode = cred->realnode;
+  *linknodetype = MACH_MSG_TYPE_COPY_SEND;
+  return 0;
+}
