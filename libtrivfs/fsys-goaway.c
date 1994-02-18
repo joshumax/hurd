3c0b1fde5e35f4fa1337d605faca3364de4f7a5e
Michael I. Bushnell <mib@gnu.org>
1994-02-18 20:59:34 +0000

    Initial revision

---
+++ libtrivfs/fsys-goaway.c
@@ -0,0 +1,32 @@
+/*
+   Copyright (C) 1994 Free Software Foundation
+
+This file is part of the GNU Hurd.
+
+The GNU Hurd is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+The GNU Hurd is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with the GNU Hurd; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+/* Written by Michael I. Bushnell.  */
+
+#include "priv.h"
+
+error_t
+trivfs_S_fsys_goaway (mach_port_t fsys,
+		      int flags)
+{
+  if (!ports_get_port (fsys))
+    return EOPNOTSUPP;
+
+  return trivfs_goaway (flags);
+}
