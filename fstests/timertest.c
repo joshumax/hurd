cfe3c52731bf64bcc264ef2abccea457896563de
Michael I. Bushnell <mib@gnu.org>
1994-05-04 17:56:04 +0000

    Initial revision

---
+++ fstests/timertest.c
@@ -0,0 +1,47 @@
+/* 
+   Copyright (C) 1994 Free Software Foundation
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+
+
+int
+alarm_handler ()
+{
+  printf ("Received alarm\n");
+  fflush (stdout);
+}
+
+main()
+{
+  struct itimerval real_timer;
+  error_t err;
+
+  real_timer.it_interval.tv_usec = 0;
+  real_timer.it_interval.tv_sec = 1;
+  real_timer.it_value.tv_usec = 0;
+  real_timer.it_value.tv_sec = 1;
+  
+  signal (SIGALRM, alarm_handler);
+  
+  err = setitimer (ITIMER_REAL, &real_timer, 0);
+  
+  while (1)
+    {
+      printf ("Pausing\n");
+      fflush (stdout);
+      sigpause (0);
+    }
+}
+
