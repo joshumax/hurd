d8342902f3261db926898372c48a4b63f9124c99
Roland McGrath <roland@gnu.org>
1994-05-05 13:14:37 +0000

    Formerly file-get-transcntl.c.~5~

--- libdiskfs/file-get-transcntl.c
+++ libdiskfs/file-get-transcntl.c
@@ -19,7 +19,7 @@
 #include "fs_S.h"
 
 /* Implement file_get_translator_cntl as described in <hurd/fs.defs>. */
-error_t
+kern_return_t
 diskfs_S_file_get_translator_cntl (struct protid *cred,
 				   mach_port_t *ctl)
 {
