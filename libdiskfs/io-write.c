738c3355af673e70f42a732f9ccfd9113c1b5358
Roland McGrath <roland@gnu.org>
1994-05-05 13:13:52 +0000

    Formerly io-write.c.~4~

--- libdiskfs/io-write.c
+++ libdiskfs/io-write.c
@@ -20,7 +20,7 @@
 #include <fcntl.h>
 
 /* Implement io_write as described in <hurd/io.defs>. */
-error_t
+kern_return_t
 diskfs_S_io_write(struct protid *cred,
 		  char *data,
 		  unsigned int datalen,
