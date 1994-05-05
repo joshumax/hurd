fdcba05178abd1af9493ad0f9e262908df49d8ec
Roland McGrath <roland@gnu.org>
1994-05-05 13:14:50 +0000

    Formerly dir-link.c.~4~

--- libdiskfs/dir-link.c
+++ libdiskfs/dir-link.c
@@ -19,7 +19,7 @@
 #include "fs_S.h"
 
 /* Implement dir_link as described in <hurd/fs.defs>. */
-error_t
+kern_return_t
 diskfs_S_dir_link (struct protid *filecred,
 		   struct protid *dircred,
 		   char *name)
