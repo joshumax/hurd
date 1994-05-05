a159878e3d0bca8d12da942bfe3df5525176a316
Roland McGrath <roland@gnu.org>
1994-05-05 13:14:46 +0000

    Formerly dir-readdir.c.~3~

--- libdiskfs/dir-readdir.c
+++ libdiskfs/dir-readdir.c
@@ -23,7 +23,7 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #include "fs_S.h"
 #include "io_S.h"
 
-error_t
+kern_return_t
 diskfs_S_dir_readdir (struct protid *cred,
 		      char **data,
 		      u_int *datacnt,
