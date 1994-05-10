749fcd79933e95a49f5c941adea55f3fa81653ca
Michael I. Bushnell <mib@gnu.org>
1994-05-10 19:38:53 +0000

    Initial revision

---
+++ libtrivfs/handle-port.c
@@ -0,0 +1,26 @@
+/* 
+   Copyright (C) 1994 Free Software Foundation
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+
+mach_port_t
+trivfs_handle_port (mach_port_t realnode)
+{
+  struct trivfs_control *cntl;
+  
+  cntl = ports_allocate_port (sizeof (struct control), trivfs_cntl_porttype);
+  cntl->underlying = realnode;
+  return ports_get_right (cntl);
+}
