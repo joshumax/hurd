86ef13055e1300a784b35f0fc70cbf13453c16ba
Michael I. Bushnell <mib@gnu.org>
1994-05-24 21:30:41 +0000

    Formerly io-owner-get.c.~2~

--- libtrivfs/io-owner-get.c
+++ libtrivfs/io-owner-get.c
@@ -20,9 +20,11 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 /* Written by Michael I. Bushnell.  */
 
 #include "priv.h"
+#include "io_S.h"
+#include <assert.h>
 
-error_t
-trivfs_S_io_get_owner (struct protid *cred,
+kern_return_t
+trivfs_S_io_get_owner (struct trivfs_protid *cred,
 		       pid_t *owner)
 {
   assert (!trivfs_support_read && !trivfs_support_write);
