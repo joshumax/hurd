3a14ab9fac5a6ce81ba1d59efc96b16953224a68
Michael I. Bushnell <mib@gnu.org>
1994-05-24 19:34:42 +0000

    Initial revision

---
+++ libtrivfs/fsmutations.h
@@ -0,0 +1,28 @@
+/* 
+   Copyright (C) 1994 Free Software Foundation
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+
+/* Only CPP macro definitions should go int his file. */
+
+#define FILE_INTRAN protid_t convert_port_to_protid (file_t)
+
+#define IO_INTRAN protid_t convert_port_to_protid (io_t)
+
+#define SERVERPREFIX trivfs_S_
+
+#define FILE_IMPORTS import "trivfs.h";
+#define IO_IMPORTS import "trivfs.h";
+#define FSYS_IMPORTS import "trvifs.h";
