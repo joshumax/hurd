0c696dc2c8d43f2353cc0078b24807a9eaf1ce45
Michael I. Bushnell <mib@gnu.org>
1994-05-24 20:24:55 +0000

    Formerly file-chmod.c.~4~

--- libtrivfs/file-chmod.c
+++ libtrivfs/file-chmod.c
@@ -16,9 +16,10 @@
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
 
 #include "priv.h"
+#include "fs_S.h"
 
 error_t
-trivfs_S_file_chmod (struct protid *cred,
+trivfs_S_file_chmod (struct trivfs_protid *cred,
 		     mode_t mode)
 {
   /* Is this right? */
