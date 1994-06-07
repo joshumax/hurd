82303b7dc8c8b4458c6c0b25d07a3b76e6709a7a
Roland McGrath <roland@gnu.org>
1994-06-07 03:06:58 +0000

    Formerly ioctl_types.h.~3~

--- hurd/ioctl_types.h
+++ hurd/ioctl_types.h
@@ -15,6 +15,6 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
 
-typedef int *modes_t, *speeds_t;
-typedef short *sizes_t;
-typedef char *ccs_t;
+typedef int modes_t[4], speeds_t[2];
+typedef short sizes_t[4];
+typedef char ccs_t[20];
