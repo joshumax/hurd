10b7bb581a4fb199f6ef1f4cbc4e16d951ff92c9
Michael I. Bushnell <mib@gnu.org>
1994-07-05 17:38:03 +0000

    Formerly io-select.c.~4~

--- libtrivfs/io-select.c
+++ libtrivfs/io-select.c
@@ -25,6 +25,8 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 kern_return_t
 trivfs_S_io_select (struct trivfs_protid *cred,
+		    mach_port_t reply,
+		    mach_msg_type_name_t replytype,
 		    int seltype,
 		    mach_port_t ret,
 		    int tag,
