b2796cf54890e6c3388231b38a690402fae0f735
Michael I. Bushnell <mib@gnu.org>
1994-07-05 17:26:36 +0000

    Formerly dir-mkdir.c.~3~

--- libtrivfs/dir-mkdir.c
+++ libtrivfs/dir-mkdir.c
@@ -19,7 +19,10 @@
 #include "fs_S.h"
 
 kern_return_t
-trivfs_S_dir_mkdir (struct trivfs_protid *cred, char *name, mode_t mode)
+trivfs_S_dir_mkdir (struct trivfs_protid *cred, 
+		    mach_port_t reply,
+		    mach_msg_type_name_t replytype,
+		    char *name, mode_t mode)
 {
   return cred ? ENOTDIR : EOPNOTSUPP;
 }
