2917d2bb5488071ccaf1726f518aaaecc34fb201
Michael I. Bushnell <mib@gnu.org>
1994-07-22 15:06:18 +0000

    Formerly host.c.~5~

--- proc/host.c
+++ proc/host.c
@@ -33,7 +33,7 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #include <hurd/version.h>
 
 #include "proc.h"
-#include "proc_S.h"
+#include "process_S.h"
 
 static long hostid;
 static char *hostname;
