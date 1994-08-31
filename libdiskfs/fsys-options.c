412e5c6a04c0aadeadf7c65752c6c2db3b145bd5
Michael I. Bushnell <mib@gnu.org>
1994-08-31 16:05:26 +0000

    entered into RCS

---
+++ libdiskfs/fsys-options.c
@@ -0,0 +1,31 @@
+/* 
+   Copyright (C) 1994 Free Software Foundation, Inc.
+   Written by Michael I. Bushnell.
+
+   This file is part of the GNU Hurd.
+
+   The GNU Hurd is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   The GNU Hurd is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+
+#include "priv.h"
+#include "fsys_S.h"
+
+/* Implement fsys_mod_readonly as described in <hurd/fsys.defs>. */
+kern_return_t
+diskfs_S_fsys_mod_readonly (fsys_t control,
+			    int val,
+			    int force)
+{
+  return EOPNOTSUPP;
+}
