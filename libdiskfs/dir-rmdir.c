acc0e47f4704e2192b1e58f3eff777a3343c9945
Michael I. Bushnell <mib@gnu.org>
1995-03-09 01:53:19 +0000

    (diskfs_S_dir_rmdir): Implement diskfs_synchronous.

--- libdiskfs/dir-rmdir.c
+++ libdiskfs/dir-rmdir.c
@@ -1,5 +1,5 @@
 /* libdsikfs implementation of fs.defs: dir_rmdir
-   Copyright (C) 1992, 1993, 1994 Free Software Foundation
+   Copyright (C) 1992, 1993, 1994, 1995 Free Software Foundation
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License as
@@ -68,11 +68,15 @@ diskfs_S_dir_rmdir (struct protid *dircred,
     }
 
   error = diskfs_dirremove (dnp, ds);
+  if (diskfs_synchronous)
+    diskfs_node_update (dnp, 1);
 
   if (!error)
     {
       np->dn_stat.st_nlink--;
       np->dn_set_ctime = 1;
+      if (diskfs_synchronous)
+	diskfs_node_update (np, 1);
     }
   
   if (!error)
