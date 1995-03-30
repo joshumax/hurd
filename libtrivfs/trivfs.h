3d9a0853ee57720e8023e9a013b65696e6685506
Michael I. Bushnell <mib@gnu.org>
1995-03-30 17:27:51 +0000

    (struct trivfs_control): New member `lock'.

--- libtrivfs/trivfs.h
+++ libtrivfs/trivfs.h
@@ -1,5 +1,5 @@
 /* 
-   Copyright (C) 1994 Free Software Foundation
+   Copyright (C) 1994, 1995 Free Software Foundation
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License as
@@ -38,6 +38,7 @@ struct trivfs_peropen
 struct trivfs_control
 {
   struct port_info pi;
+  struct mutex lock;
   int protidtypes;
   mach_port_t underlying;
   struct pending_open *openshead, *openstail;
