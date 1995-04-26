521cb7c0846cf3c5b6f05c228510278c4c6fa293
Miles Bader <miles@gnu.org>
1995-04-26 22:49:34 +0000

    Formerly ext2_fs.h.~13~

--- ext2fs/ext2_fs.h
+++ ext2fs/ext2_fs.h
@@ -63,7 +63,7 @@ typedef signed char s8;
  */
 #ifdef EXT2FS_DEBUG
 #	define ext2_debug(f, a...) \
-    { printf ("ext2fs DEBUG %s:%d: %s: ", __FILE__, __LINE__, __FUNCTION__);
+    { printf ("ext2fs: DEBUG %s:%d: %s: ", __FILE__, __LINE__, __FUNCTION__); \
       printf (f, ## a); }
 #else
 #	define ext2_debug(f, a...)	/**/
