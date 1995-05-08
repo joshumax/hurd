1060d7127eba505be83ab6af9be953e6b436c005
Miles Bader <miles@gnu.org>
1995-05-08 13:43:50 +0000

    Formerly bitmap.c.~8~

--- ext2fs/bitmap.c
+++ ext2fs/bitmap.c
@@ -1,5 +1,25 @@
+/* Bitmap perusing routines.
+
+   Copyright (C) 1995 Free Software Foundation, Inc.
+
+   Converted to work under the hurd by Miles Bader <miles@gnu.ai.mit.edu>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+
 /*
- *  linux/fs/ext2/bitmap.c (etc
+ *  linux/fs/ext2/bitmap.c (&c)
  *
  * Copyright (C) 1992, 1993, 1994, 1995
  * Remy Card (card@masi.ibp.fr)
