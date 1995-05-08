bf3482f63a7d5c6026cd6c196abde7564707d835
Miles Bader <miles@gnu.org>
1995-05-08 13:38:01 +0000

    Formerly hyper.c.~11~

--- ext2fs/hyper.c
+++ ext2fs/hyper.c
@@ -2,6 +2,8 @@
 
    Copyright (C) 1994, 1995 Free Software Foundation, Inc.
 
+   Written by Miles Bader <miles@gnu.ai.mit.edu>
+
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License as
    published by the Free Software Foundation; either version 2, or (at
