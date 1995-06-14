7bf3bc93c93fe216b01c85d59667b46582009dc0
Michael I. Bushnell <mib@gnu.org>
1995-06-14 20:10:49 +0000

    Really do last change.

--- libdiskfs/priv.h
+++ libdiskfs/priv.h
@@ -72,8 +72,7 @@ extern int _diskfs_ncontrol_ports;
 extern spin_lock_t _diskfs_control_lock;
 
 /* Callback routine for active translator startup */
-error_t _diskfs_translator_callback (void *, void *, mach_port_t *, uid_t *,
-				     gid_t *, char **, int *);
+fshelp_callback_t _diskfs_translator_callback;
 
 /* This macro locks the node associated with PROTID, and then
    evaluates the expression OPERATION; then it syncs the inode
