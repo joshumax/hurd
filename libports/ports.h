1df905c3e49a620e17ff46e21307f87966174218
Michael I. Bushnell <mib@gnu.org>
1995-06-21 18:57:19 +0000

    Typo.

--- libports/ports.h
+++ libports/ports.h
@@ -171,8 +171,8 @@ void ports_enable_class (struct port_class *class);
 void port_enable_bucket (struct port_bucket *bucket);
 
 /* Call FUN once for each port in BUCKET. */
-error_t port_bucket_iterate (struct port_bucket *bucket,
-			     error_t (*fun)(void *port));
+error_t ports_bucket_iterate (struct port_bucket *bucket,
+			      error_t (*fun)(void *port));
 
 
 
