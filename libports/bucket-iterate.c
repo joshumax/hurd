6b82db63cc26425fe0ac66f21bb7f934a2fa86bc
Michael I. Bushnell <mib@gnu.org>
1995-06-21 18:36:48 +0000

    Initial revision

---
+++ libports/bucket-iterate.c
@@ -0,0 +1,59 @@
+/* 
+   Copyright (C) 1995 Free Software Foundation, Inc.
+   Written by Michael I. Bushnell.
+
+   This file is part of the GNU Hurd.
+
+   The GNU Hurd is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   The GNU Hurd is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA. */
+
+#include "ports.h"
+
+/* This is obsecenely ineffecient.  ihash and ports need to cooperate
+   more closely to do it effeciently. */
+void
+ports_bucket_iterate (struct port_bucket *bucket,
+		      void (*fun)(void *))
+{
+  struct item 
+    {
+      struct item *next;
+      void *p;
+    } *list = 0;
+  struct item *i, nxt;
+
+  error_t enqueue (void *pi)
+    {
+      struct item *j;
+      
+      j = malloc (sizeof (struct item));
+      j->next = list;
+      j->p = pi;
+      list = j;
+      ((struct port_info *)pi)->refcnt++;
+      return 0;
+    }
+  
+  mutex_lock (&_ports_lock);
+  ihash_iterate (bucket->htable, enqueue);
+  mutex_unlock (&_ports_lock);
+  
+  for (i = list; i; i = nxt)
+    {
+      (*fun)(i->p);
+      ports_port_deref (i->p);
+      nxt = i->next;
+      free (i);
+    }
+}  
