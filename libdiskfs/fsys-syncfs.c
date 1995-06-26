18264abd92fc61c122fa090d4e2f4474d33a82b1
Michael I. Bushnell <mib@gnu.org>
1995-06-26 23:41:20 +0000

    (diskfs_S_fsys_syncfs): New parms REPLY and REPLYTYPE.

--- libdiskfs/fsys-syncfs.c
+++ libdiskfs/fsys-syncfs.c
@@ -25,6 +25,8 @@
 /* Implement fsys_syncfs as described in <hurd/fsys.defs>. */
 kern_return_t
 diskfs_S_fsys_syncfs (fsys_t controlport,
+		      mach_port_t reply,
+		      mach_msg_type_name_t replytype,
 		      int wait,
 		      int children)
 {
