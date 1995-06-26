09d5b857178387e44dda0eaa40b711fc689c70e1
Michael I. Bushnell <mib@gnu.org>
1995-06-26 23:33:36 +0000

    (diskfs_S_fsys_set_options): New parms REPLY and REPLYTYPE.

--- libdiskfs/fsys-options.c
+++ libdiskfs/fsys-options.c
@@ -28,6 +28,8 @@
 /* Implement fsys_set_options as described in <hurd/fsys.defs>. */
 kern_return_t
 diskfs_S_fsys_set_options (fsys_t fsys,
+			   mach_port_t reply,
+			   mach_msg_type_name_t replytype,
 			   char *data, mach_msg_type_number_t len,
 			   int do_children)
 {
