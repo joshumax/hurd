5774ec100ec222ae55ba081b9fc9a5419dc3b9e8
Michael I. Bushnell <mib@gnu.org>
1995-07-07 02:07:56 +0000

    Include <sys/ioctl.h>.

--- boot/tcattr.c
+++ boot/tcattr.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1993, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -19,6 +19,7 @@ Cambridge, MA 02139, USA.  */
 #include <errno.h>
 #include <stddef.h>
 #include <termios.h>
+#include <sys/ioctl.h>
 
 #undef	B0
 #undef	B50
