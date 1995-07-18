311f82235641b85a35de3e05dcced843b8b7e6fc
Miles Bader <miles@gnu.org>
1995-07-18 18:38:02 +0000

    Formerly sock.h.~6~

--- pflocal/sock.h
+++ pflocal/sock.h
@@ -141,6 +141,10 @@ error_t addr_create (struct addr **addr);
    sock will have one reference added to it.  */
 error_t addr_get_sock (struct addr *addr, struct sock **sock);
 
+/* Try to shutdown any active sockets, returning EBUSY if we can't.  Assumes
+   RPCS's have been disabled.  */
+error_t sock_goaway (int flags);
+
 extern struct port_class *sock_user_port_class;
 extern struct port_class *addr_port_class;
 
