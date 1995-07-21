a1bc2195b1cc130a53df9ff19fb60e4f6c781e43
Michael I. Bushnell <mib@gnu.org>
1995-07-21 17:30:16 +0000

    (diskfs_S_dir_lookup): Examine the active translator on NP, not on diskfs_root_node, to see if translator usage is necessary.

--- libdiskfs/dir-lookup.c
+++ libdiskfs/dir-lookup.c
@@ -170,8 +170,7 @@ diskfs_S_dir_lookup (struct protid *dircred,
       /* If this is translated, start the translator (if necessary)
 	 and return.  */
       if ((((flags & O_NOTRANS) == 0) || !lastcomp)
-	  && (np->istranslated
-	      || fshelp_translated (&diskfs_root_node->transbox)))
+	  && (np->istranslated || fshelp_translated (&np->transbox)))
 	{
 	  mach_port_t dirport;
 	  
