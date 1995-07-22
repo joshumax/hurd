9d47d898acc7878d9420b364a2e953319683d17f
Miles Bader <miles@gnu.org>
1995-07-22 04:48:48 +0000

    Formerly sserver.c.~3~

--- pflocal/sserver.c
+++ pflocal/sserver.c
@@ -1,7 +1,35 @@
-/* ---------------------------------------------------------------- */
+/* Server for socket ops
+
+   Copyright (C) 1995 Free Software Foundation, Inc.
+
+   Written by Miles Bader <miles@gnu.ai.mit.edu>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+
+#include <cthreads.h>
+
+#include <hurd/ports.h>
+
+#include "notify_S.h"
+
+#include "debug.h"
 
 /* A port bucket to handle SOCK_USERs and ADDRs.  */
 struct port_bucket *sock_port_bucket;
+
+/* ---------------------------------------------------------------- */
 
 /* True if there are threads servicing sock requests.  */
 static int sock_server_active = 0;
@@ -69,8 +97,8 @@ error_t
 do_mach_notify_no_senders (mach_port_t port, mach_port_mscount_t count)
 {
   void *pi = ports_lookup_port (sock_port_bucket, port, 0);
-debug (pi, "count: %ul, refs: %d",
-       count, (pi ? ((struct port_info *)pi->refcnt) : 0));
+debug (pi, "count: %lu, refs: %d",
+       count, (pi ? ((struct port_info *)pi)->refcnt : 0));
   if (!pi)
     return EOPNOTSUPP;
   ports_no_senders (pi, count);
