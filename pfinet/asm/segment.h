a378d612d2f809728ae7329274cb4440639620c3
Michael I. Bushnell <mib@gnu.org>
1995-08-03 16:13:20 +0000

    Formerly segment.h.~3~

--- pfinet/asm/segment.h
+++ pfinet/asm/segment.h
@@ -1,11 +1,17 @@
 #ifndef _HACK_ASM_SEGMENT_H_
 #define _HACK_ASM_SEGMENT_H_
 
+#include <sys/types.h>
+
 #define get_fs_long(addr) get_user_long((int *)(addr))
 
-u_long get_user_long (const int *addr);
+unsigned long get_user_long (const int *addr);
 
 #define put_fs_long(x,addr) put_user_long((x),(int *)(addr))
 
-void put_user_long (u_long, int *);
+void put_user_long (unsigned long, int *);
 
+void memcpy_fromfs (void *, void *, size_t);
+void memcpy_tofs (void *, void *, size_t);
+
+#endif
