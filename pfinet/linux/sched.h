3db57cad20cbd1ab027d08bb410ec6cbd16eb976
Michael I. Bushnell <mib@gnu.org>
1995-08-03 18:33:32 +0000

    Formerly sched.h.~3~

--- pfinet/linux/sched.h
+++ pfinet/linux/sched.h
@@ -3,6 +3,7 @@
 
 #include <linux/wait.h>
 #include <sys/signal.h>
+#include <hurd/hurd_types.h>
 
 extern unsigned long volatile jiffies;
 #define HZ 100
@@ -23,10 +24,18 @@ struct task_struct
 void wake_up_interruptible (struct wait_queue **);
 void interruptible_sleep_on (struct wait_queue **);
 
+void select_wait (struct wait_queue **, select_table *);
+
+#define SEL_IN SELECT_READ
+#define SEL_OUT SELECT_WRITE
+#define SEL_EX SELECT_URG
+
 int send_sig (u_long, struct task_struct *, int);
 
 int fetch_current_time (void);
+struct timeval fetch_xtime (void);
 
 #define CURRENT_TIME (fetch_current_time())
+#define xtime (fetch_xtime ())
 
 #endif
