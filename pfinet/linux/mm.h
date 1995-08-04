f1e3c7aeb62d23215f3913722f97ccc1dc1c4003
Michael I. Bushnell <mib@gnu.org>
1995-08-04 18:39:20 +0000

    Formerly mm.h.~4~

--- pfinet/linux/mm.h
+++ pfinet/linux/mm.h
@@ -1,10 +1,14 @@
 #ifndef _HACK_MM_H_
 #define _HACK_MM_H_
 
+#include <linux/kernel.h>
+#include <linux/sched.h>
+
 int verify_area (int, const void *, u_long);
 
 #define VERIFY_READ 0
 #define VERIFY_WRITE 0
 #define GFP_ATOMIC 0
+#define GFP_KERNEL 0
 
 #endif
