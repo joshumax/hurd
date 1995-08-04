6430e733ad662ad8db534e4020c7b6da73b2d3b4
Michael I. Bushnell <mib@gnu.org>
1995-08-04 19:51:57 +0000

    Formerly segment.h.~4~

--- pfinet/asm/segment.h
+++ pfinet/asm/segment.h
@@ -4,12 +4,13 @@
 #include <sys/types.h>
 
 #define get_fs_long(addr) get_user_long((int *)(addr))
-
 unsigned long get_user_long (const int *addr);
 
 #define put_fs_long(x,addr) put_user_long((x),(int *)(addr))
-
 void put_user_long (unsigned long, int *);
+
+#define put_fs_byte(x,addr) put_user_byte ((x),(char *)(addr))
+void put_user_byte (char, char *);
 
 void memcpy_fromfs (void *, void *, size_t);
 void memcpy_tofs (void *, void *, size_t);
