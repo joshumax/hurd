c0a0ebf04e31c19063912dbb143e33ad7082083b
Michael I. Bushnell <mib@gnu.org>
1995-08-08 17:28:41 +0000

    Formerly kernel.h.~4~

--- pfinet/linux/kernel.h
+++ pfinet/linux/kernel.h
@@ -2,13 +2,21 @@
 #define _HACK_KERNEL_H
 
 #include <stdio.h>
+#include <linux/sched.h>
 
 #define printk printf
 
 int getname (const char *, char **);
 void putname (char *);
 
-int suser (void);
+extern inline int suser ()
+{
+  return current->isroot;
+};
+
+
+int kill_proc (int, int, int);
+int kill_pg (int, int, int);
 
 
 #endif
