42109ab3c2bb6e5787ad2beecbf5cfa7fdd79e48
Michael I. Bushnell <mib@gnu.org>
1995-08-08 15:36:42 +0000

    Initial revision

---
+++ pfinet/config.h
@@ -0,0 +1,11 @@
+#define CONFIG_INET
+#define CONFIG_NET
+
+/* #undef CONFIG_INET_RARP */
+#define CONFIG_IP_MULTICAST
+
+
+/* #undef CONFIG_TCP_NAGLE_OFF */
+/* #undef CONFIG_AX25 */
+/* #undef CONFIG_IPX */
+/* #undef CONFIG_ATALK */
