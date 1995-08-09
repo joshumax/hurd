cfe5ef951dd1fe7b7a22e594145ca780df146f4c
Michael I. Bushnell <mib@gnu.org>
1995-08-09 16:50:49 +0000

    entered into RCS

--- pfinet/linux/time.h
+++ pfinet/linux/time.h
@@ -1,7 +1,13 @@
 #ifndef _HACK_TIME_H_
+#define _HACK_TIME_H_
 
 #include <sys/time.h>
+#include "mapped-time.h"
 
-void do_gettimeofday (struct timeval *);
+extern inline void 
+do_gettimeofday (struct timeval *tp)
+{
+  fill_timeval (tp);
+}
 
 #endif
