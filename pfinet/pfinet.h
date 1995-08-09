4b8644ef26011e3ebf4740a0630a8c5dd4916d43
Michael I. Bushnell <mib@gnu.org>
1995-08-09 17:16:44 +0000

    Formerly pfinet.h.~2~

--- pfinet/pfinet.h
+++ pfinet/pfinet.h
@@ -18,4 +18,6 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA. */
 
-device_t master_device;
+#include <device/device.h>
+
+extern device_t master_device;
