886b2fb87c7e7066c918b38a9b3b122505a910a1
Michael I. Bushnell <mib@gnu.org>
1995-08-16 15:54:05 +0000

    Formerly sched.c.~5~

--- pfinet/sched.c
+++ pfinet/sched.c
@@ -19,6 +19,7 @@
    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA. */
 
 #include <asm/system.h>
+#include <linux/sched.h>
 
 struct mutex global_interrupt_lock = MUTEX_INITIALIZER;
 
