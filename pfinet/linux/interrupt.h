e209db9b661ed65761e420c43a5763493e570618
Michael I. Bushnell <mib@gnu.org>
1995-08-16 17:23:36 +0000

    Formerly interrupt.h.~4~

--- pfinet/linux/interrupt.h
+++ pfinet/linux/interrupt.h
@@ -1,15 +1,16 @@
 #ifndef _HACK_INTERRUPT_H_
 #define _HACK_INTERRUPT_H_
 
+#include "pfinet.h"
+
 #define NET_BH 1
 
 extern inline void 
-mark_bottom_half (int foo)
+mark_bh (int foo)
 {
   assert (foo == NET_BH);
   
   incoming_net_packet ();
 }
-
 
 #endif
