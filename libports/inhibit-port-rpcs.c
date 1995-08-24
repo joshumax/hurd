5afd7d8a58fc5f27d8c6385abd9657f84c37f084
Miles Bader <miles@gnu.org>
1995-08-24 19:48:26 +0000

    (inhibit_port_rpcs): thread_cancel -> hurd_thread_cancel.  Include <hurd.h>.

--- libports/inhibit-port-rpcs.c
+++ libports/inhibit-port-rpcs.c
@@ -19,6 +19,7 @@
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
 
 #include "ports.h"
+#include <hurd.h>
 #include <cthreads.h>
 
 void
@@ -30,7 +31,7 @@ inhibit_port_rpcs (void *portstruct)
   mutex_lock (&_ports_lock);
 
   for (rpc = pi->current_rpcs; rpc; rpc = rpc->next)
-    thread_cancel (rpc->thread);
+    hurd_thread_cancel (rpc->thread);
   
   while (pi->current_rpcs)
     {
