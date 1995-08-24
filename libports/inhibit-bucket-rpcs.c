b476fe5860f1a745b4a119e08dfe1ab1f026fbd7
Miles Bader <miles@gnu.org>
1995-08-24 19:49:28 +0000

    (ports_inhibit_bucket_rpcs): thread_cancel -> hurd_thread_cancel. Include <hurd.h>.

--- libports/inhibit-bucket-rpcs.c
+++ libports/inhibit-bucket-rpcs.c
@@ -19,6 +19,7 @@
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
 
 #include "ports.h"
+#include <hurd.h>
 #include <cthreads.h>
 #include <hurd/ihash.h>
 
@@ -31,7 +32,7 @@ ports_inhibit_bucket_rpcs (struct port_bucket *bucket)
       struct rpc_info *rpc;
       
       for (rpc = pi->current_rpcs; rpc; rpc = rpc->next)
-	thread_cancel (rpc->thread);
+	hurd_thread_cancel (rpc->thread);
       return 0;
     }
   
