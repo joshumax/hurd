6db64ebf26f6fd64aa0b86940fe7ab74be0d45cb
Roland McGrath <roland@gnu.org>
1995-09-17 21:57:42 +0000

    (EXEC_STACK_ARGS): New macro.

--- hurd/hurd_types.h
+++ hurd/hurd_types.h
@@ -71,7 +71,9 @@ typedef struct stat io_statbuf_t;
 #define EXEC_NEWTASK	0x00000001 /* Create new task; kill old one.  */
 #define EXEC_SECURE	0x00000002 /* Use secure values of portarray, etc. */
 #define EXEC_DEFAULTS	0x00000004 /* Use defaults for unspecified ports.  */
+/* These two are passed through by the exec server but not examined by it.  */
 #define	EXEC_TRACED	0x00000008 /* Process is traced.  */
+#define	EXEC_STACK_ARGS	0x00000010 /* Use arguments from stack, not RPC.  */
 #define	EXEC_INHERITED	(EXEC_TRACED) /* Flags inherited by later execs.  */
 
 /* Bits for flags in fs.defs:file_set_translator call: */
