5a9d5572de986d51df7f2ac52770a9dee8ce2cdc
Miles Bader <miles@gnu.org>
1995-10-14 22:47:06 +0000

    Initial revision

---
+++ libstore/rdwr.c
@@ -0,0 +1,34 @@
+/* Store I/O
+
+   Copyright (C) 1995 Free Software Foundation, Inc.
+
+   Written by Miles Bader <miles@gnu.ai.mit.edu>
+
+   This file is part of the GNU Hurd.
+
+   The GNU Hurd is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   The GNU Hurd is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   675 Mass Ave, Cambridge, MA 02139, USA. */
+
+error_t
+store_write (struct store *store,
+	     off_t addr, char *buf, size_t len, size_t *amount);
+{
+  
+}
+
+error_t
+store_read (struct store *store,
+	    off_t addr, size_t amount, char **buf, size_t *len)
+{
+}
