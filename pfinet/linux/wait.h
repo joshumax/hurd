9bf42fd45219b511702a7fe75ea66f8cab9c14c6
Michael I. Bushnell <mib@gnu.org>
1995-11-01 20:51:01 +0000

    entered into RCS

--- pfinet/linux/wait.h
+++ pfinet/linux/wait.h
@@ -8,8 +8,16 @@ struct wait_queue
   struct condition c;
 };
 
+struct select_table_elt
+{
+  struct condition *dependent_condition;
+  struct select_table_elt *next;
+};
+
 typedef struct select_table_struct
 {
+  struct condition master_condition;
+  struct select_table_elt *head;
 } select_table;
 
 #endif
