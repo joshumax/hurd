a7819e462d30a7d2e0b1319adb54d1184d0dc186
Miles Bader <miles@gnu.org>
1995-11-06 21:10:07 +0000

    (S_socket_create_address): Removing BINDING argument.

--- pflocal/pf.c
+++ pflocal/pf.c
@@ -66,12 +66,10 @@ S_socket_create (mach_port_t pf,
 }
 
 error_t
-S_socket_create_address (mach_port_t pf,
-			 int sockaddr_type,
+S_socket_create_address (mach_port_t pf, int sockaddr_type,
 			 char *data, size_t data_len,
 			 mach_port_t *addr_port,
-			 mach_msg_type_name_t *addr_port_type,
-			 int binding)
+			 mach_msg_type_name_t *addr_port_type)
 {
   return EOPNOTSUPP;
 }
