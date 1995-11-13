76d865eff89fbc81134af4aefd0b42618bb038a3
Michael I. Bushnell <mib@gnu.org>
1995-11-13 21:45:05 +0000

    Formerly file-utimes.c.~2~

--- libnetfs/file-utimes.c
+++ libnetfs/file-utimes.c
@@ -38,8 +38,7 @@ netfs_S_file_utimes (struct protid *user,
     return EOPNOTSUPP;
   
   mutex_lock (&user->po->np->lock);
-  err = netfs_attempt_chown (user->credential, user->po->np,
-			     uid, gid);
+  err = netfs_attempt_utimes (user->credential, user->po->np, &atime, &mtime);
   mutex_unlock (&user->po->np->lock);
   return err;
 }
