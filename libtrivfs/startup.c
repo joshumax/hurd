cfbfdb8268c40641bf98b85a7cb3e7f4f65cdafe
Miles Bader <miles@gnu.org>
1995-11-13 18:47:46 +0000

    (trivfs_startup): Add FLAGS arg, passed to fsys_startup.

--- libtrivfs/startup.c
+++ libtrivfs/startup.c
@@ -28,9 +28,10 @@
    fsys_startup.  CONTROL_TYPE is the ports library type for the control
    port, and PROTID_TYPE is the type for ports representing opens of this
    node.  If CONTROL isn't NULL, the trivfs control port is return in it.  If
-   any error occurs sending fsys_startup, it is returned, otherwise 0.  */
+   any error occurs sending fsys_startup, it is returned, otherwise 0.
+   FLAGS specifies how to open the underlying node (O_*).  */
 error_t
-trivfs_startup(mach_port_t bootstrap,
+trivfs_startup(mach_port_t bootstrap, int flags,
 	       struct port_class *control_class,
 	       struct port_bucket *control_bucket,
 	       struct port_class *protid_class,
@@ -47,7 +48,8 @@ trivfs_startup(mach_port_t bootstrap,
   assert(mcntl != MACH_PORT_NULL);
 
   /* Contact whoever started us.  */
-  err = fsys_startup (bootstrap, mcntl, MACH_MSG_TYPE_MAKE_SEND, &realnode);
+  err =
+    fsys_startup (bootstrap, flags, mcntl, MACH_MSG_TYPE_MAKE_SEND, &realnode);
 
   /* Install the returned realnode for trivfs's use */
   tcntl = ports_lookup_port (control_bucket, mcntl, control_class);
