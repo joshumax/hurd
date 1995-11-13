275ead4b88aae34642eba6f6e37cbdfed61e47d8
Michael I. Bushnell <mib@gnu.org>
1995-11-13 21:27:25 +0000

    Initial revision

---
+++ libnetfs/io-readable.c
@@ -0,0 +1,37 @@
+/* 
+   Copyright (C) 1995 Free Software Foundation, Inc.
+   Written by Michael I. Bushnell, p/BSG.
+
+   This file is part of the GNU Hurd.
+
+   The GNU Hurd is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   The GNU Hurd is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA. */
+
+#include "netfs.h"
+
+error_t
+netfs_S_io_readable (struct protid *user,
+		     mach_msg_type_number_t *amount)
+{
+  if (!user)
+    return EOPNOTSUPP;
+  
+  if (!(cred->po->openstat & O_READ))
+    return EINVAL;
+  
+  mutex_lock (&user->po->np->lock);
+  *amount = user->po->np->nn_stat.st_size - user->po->filepointer;
+  mutex_unlock (&user->po->np->lock);
+  return 0;
+}
---
+++ libnetfs/io-seek.c
@@ -0,0 +1,56 @@
+/* 
+   Copyright (C) 1995 Free Software Foundation, Inc.
+   Written by Michael I. Bushnell, p/BSG.
+
+   This file is part of the GNU Hurd.
+
+   The GNU Hurd is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   The GNU Hurd is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA. */
+
+#include "netfs.h"
+
+error_t
+netfs_S_io_seek (struct protid *user,
+		 off_t offset,
+		 int whence,
+		 off_t *newoffset)
+{
+  if (!user)
+    return EOPNOTSUPP;
+
+  mutex_lock (&user->po->np->lock);
+  switch (whence)
+    {
+    case SEEK_SET:
+      err = 0;
+      cred->po->filepointer = offset;
+      break;
+
+    case SEEK_CUR:
+      err = 0;
+      cred->po->filepointer += offset;
+      break;
+      
+    case SEEK_END:
+      err = 0;
+      cred->po->filepointer = cred->po->np->nn_stat.st_size + offset;
+      break;
+      
+    default:
+      err = EINVAL;
+      break;
+    }
+  mutex_unlock (&user->po->np->lock);
+  return err;
+}
