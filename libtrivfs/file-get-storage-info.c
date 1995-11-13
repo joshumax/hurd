15289ea81c71e4f606689d34721b9ccf08f73c59
Miles Bader <miles@gnu.org>
1995-11-13 18:46:21 +0000

    (trivfs_S_file_get_storage_info):   Add FLAGS arg.   Waste some time adding reply port args that no one will use.

--- libtrivfs/file-get-storage-info.c
+++ libtrivfs/file-get-storage-info.c
@@ -19,12 +19,15 @@
 #include "fs_S.h"
 
 error_t
-trivfs_S_file_get_storage_info (struct trivfs_protid *cred, int *class,
+trivfs_S_file_get_storage_info (struct trivfs_protid *cred,
+				mach_port_t reply, mach_msg_type_name_t reply_type,
+				int *class,
 				off_t **runs, mach_msg_type_number_t *runs_len,
 				size_t *block_size,
 				char *dev_name, mach_port_t *dev_port,
 				mach_msg_type_name_t *dev_port_type,
-				char **misc, mach_msg_type_number_t *misc_len)
+				char **misc, mach_msg_type_number_t *misc_len,
+				int *flags)
 {
   return EOPNOTSUPP;
 }
