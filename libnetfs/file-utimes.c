27d42723c4c06dcd73e32f963247ceda6f43c7a5
Michael I. Bushnell <mib@gnu.org>
1995-11-13 21:44:31 +0000

    Initial revision

---
+++ libnetfs/file-utimes.c
@@ -0,0 +1,45 @@
+/* 
+   Copyright (C) 1995 Free Software Foundation, Inc.
+   Written by Michael I. Bushnell, p/BSG.
+
+   This file is part of the GNU Hurd.
+
+   The GNU Hurd is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   The GNU Hurd is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA. */
+
+#include "netfs.h"
+#include "fs_S.h"
+
+error_t
+netfs_S_file_utimes (struct protid *user,
+		     time_value_t atimein,
+		     time_value_t mtimein)
+{
+  struct timespec atime, mtime;
+  error_t err;
+  
+  atime.ts_secs = atimein.seconds;
+  atime.ts_nsecs = atimein.microseconds * 1000;
+  mtime.ts_secs = mtimein.seconds;
+  mtime.ts_nsecs = mtimein.microseconds * 1000;
+  
+  if (!user)
+    return EOPNOTSUPP;
+  
+  mutex_lock (&user->po->np->lock);
+  err = netfs_attempt_chown (user->credential, user->po->np,
+			     uid, gid);
+  mutex_unlock (&user->po->np->lock);
+  return err;
+}
