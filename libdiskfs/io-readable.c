3fd0286bd2761864cf3ee39da705fdb50f06dedf
Michael I. Bushnell <mib@gnu.org>
1995-11-13 21:28:23 +0000

    (diskfs_S_io_readable): Set *AMOUNT to zero if filepointer is past the end of the file.

--- libdiskfs/io-readable.c
+++ libdiskfs/io-readable.c
@@ -1,5 +1,5 @@
 /* 
-   Copyright (C) 1994 Free Software Foundation
+   Copyright (C) 1994, 1995 Free Software Foundation
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License as
@@ -36,7 +36,10 @@ diskfs_S_io_readable (struct protid *cred,
   
   mutex_lock (&np->lock);
   ioserver_get_conch (&np->conch);
-  *amount = np->dn_stat.st_size - cred->po->filepointer;
+  if (np->dn_stat.st_size > cred->po->filepointer)
+    *amount = np->dn_stat.st_size - cred->po->filepointer;
+  else
+    *amount = 0;
 
   mutex_unlock (&np->lock);
   return 0;
