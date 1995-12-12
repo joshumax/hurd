946f42128b5b496452d711b9575b69457509ad9b
Michael I. Bushnell <mib@gnu.org>
1995-12-12 21:17:39 +0000

    typo.

--- term/term.h
+++ term/term.h
@@ -298,7 +298,7 @@ error_t pty_io_read (struct trivfs_protid *, char **,
 		     mach_msg_type_number_t *, mach_msg_type_number_t);
 error_t pty_io_readable (int *);
 error_t pty_io_select (struct trivfs_protid *, int *, int *);
-error_t pty_open_hook (struct trivfs_cntl, uid_t *, u_int, uid_t *, 
+error_t pty_open_hook (struct trivfs_control *, uid_t *, u_int, uid_t *, 
 		       u_int, int);
 error_t pty_po_create_hook (struct trivfs_peropen *);
 error_t pty_po_destroy_hook (struct trivfs_peropen *);
