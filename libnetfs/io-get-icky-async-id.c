0258f29de6fd5146fe4d20837b1206d60b9211e8
Michael I. Bushnell <mib@gnu.org>
1995-12-19 17:51:37 +0000

    Initial revision

---
+++ libnetfs/io-get-icky-async-id.c
@@ -0,0 +1,30 @@
+/* 
+   Copyright (C) 1995 Free Software Foundation, Inc.
+   Written by Michael I. Bushnell, p/BSG.
+
+   This file is part of the GNU Hurd.
+
+   The GNU Hurd is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   The GNU Hurd is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA. */
+
+#include "netfs.h"
+#include "io_S.h"
+
+error_t
+netfs_S_io_get_icky_async_id (struct protid *user, mach_port_t *pt,
+			      mach_mgg_type_name_t *ptt)
+{
+  return EOPNOTSUPP;
+}
+
