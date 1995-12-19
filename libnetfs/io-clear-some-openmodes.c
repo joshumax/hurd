eec4f14120299653b29f84c8122a7c8aefca2109
Michael I. Bushnell <mib@gnu.org>
1995-12-19 17:46:01 +0000

    Initial revision

---
+++ libnetfs/io-clear-some-openmodes.c
@@ -0,0 +1,34 @@
+/* 
+   Copyright (C) 1995 Free Software Foundation, Inc.
+   Written by Michael I. Bushnell, p/BSG.
+
+   This file is part of the GNU Hurd.
+
+   The GNU Hurd is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   The GNU Hurd is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA. */
+
+#include "netfs.h"
+#include "io_S.h"
+
+error_t
+netfs_S_io_clear_some_openmodes (struct protid *user, int bits)
+{
+  if (!user)
+    return EOPNOTSUPP;
+  
+  mutex_lock (&user->po->np->lock);
+  user->po->openstat &= ~(bits & HONORED_STATE_MODES);
+  mutex_unlock (&user->po->np->lock);
+  return 0;
+}
