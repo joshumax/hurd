f2f7241b0b4fe20d602de542b87a87e43d0466b9
Miles Bader <miles@gnu.org>
1996-02-15 22:52:14 +0000

    Change -aBACKUP_SHELL to -aBACKUP_SHELLS.

--- utils/sush.sh
+++ utils/sush.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #  A unix-like su (one which invokes a sub-shell).
-exec /bin/login --program-name="$0" -pzxSLf -aHOME -aMOTD -aUMASK -aBACKUP_SHELL "$@"
+exec /bin/login --program-name="$0" -pzxSLf -aHOME -aMOTD -aUMASK -aBACKUP_SHELLS "$@"
