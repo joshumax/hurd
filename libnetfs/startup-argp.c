a9df4e54ff8e61a8b42090276547b3b310dc225b
Miles Bader <miles@gnu.org>
1996-03-02 01:04:31 +0000

    Initial revision

---
+++ libnetfs/startup-argp.c
@@ -0,0 +1,50 @@
+/* Standard startup-time command line parser
+
+   Copyright (C) 1995, 1996 Free Software Foundation, Inc.
+
+   Written by Miles Bader <miles@gnu.ai.mit.edu>
+
+   This file is part of the GNU Hurd.
+
+   The GNU Hurd is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   The GNU Hurd is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+
+#include <stdio.h>
+#include <argp.h>
+
+#include "netfs.h"
+
+static const struct argp_option
+startup_options[] =
+{
+  {0}
+};
+
+static error_t
+parse_startup_opt (int opt, char *arg, struct argp_state *state)
+{
+  switch (opt)
+    {
+    default:
+      return EINVAL;
+    }
+
+  return 0;
+}
+
+static const struct argp
+startup_argp =
+  { startup_options, parse_startup_opt };
+
+const struct argp *netfs_startup_argp = &startup_argp;
---
+++ libnetfs/unparse-runtime-options.c
@@ -0,0 +1,27 @@
+/* A default netfs_unparse_runtime_options routine
+
+   Copyright (C) 1995, 1996 Free Software Foundation, Inc.
+
+   This file is part of the GNU Hurd.
+
+   The GNU Hurd is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   The GNU Hurd is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+
+#include "netfs.h"
+
+error_t
+netfs_unparse_runtime_options (char **argz, size_t *argz_len);
+{
+  return 0;
+}
