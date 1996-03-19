168234f5c6667cb4db6895e4d26199a13874c43f
Michael I. Bushnell <mib@gnu.org>
1996-03-19 20:10:47 +0000

    (diskfs_S_dir_unlink): Call diskfs_purge_cache_node before diskfs_dirremove.

--- libdiskfs/dir-unlink.c
+++ libdiskfs/dir-unlink.c
@@ -1,5 +1,5 @@
 /* libdiskfs implementation of fs.defs: dir_unlink
-   Copyright (C) 1992, 1993, 1994, 1995 Free Software Foundation
+   Copyright (C) 1992, 1993, 1994, 1995, 1996 Free Software Foundation
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License as
@@ -62,6 +62,7 @@ diskfs_S_dir_unlink (struct protid *dircred,
       return EISDIR;
     }
   
+  diskfs_purge_cache_node (dnp, np);
   error = diskfs_dirremove (dnp, ds);
   if (diskfs_synchronous)
     diskfs_node_update (dnp, 1);
