0ce7ce68d043d0177113752b7293ca74e1083718
Miles Bader <miles@gnu.org>
1996-03-22 00:42:03 +0000

    Initial revision

---
+++ libstore/decode.c
@@ -0,0 +1,33 @@
+/* Store wire decoding
+
+   Copyright (C) 1996 Free Software Foundation, Inc.
+
+   Written by Miles Bader <miles@gnu.ai.mit.edu>
+
+   This file is part of the GNU Hurd.
+
+   The GNU Hurd is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   The GNU Hurd is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+
+#include <string.h>
+
+#include "store.h"
+
+/* Decode ENC, either returning a new store in STORE, or an error.  If
+   nothing else is to be done with ENC, its contents may then be freed using
+   store_enc_dealloc.  */
+error_t
+store_decode (struct store_enc *enc, struct store *store)
+{
+}
