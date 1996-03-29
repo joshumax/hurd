4b9be91508d35bac05e39b789fdf0d401e69402b
Miles Bader <miles@gnu.org>
1996-03-29 00:30:35 +0000

    Fix function name typos.

--- libshouldbeinlibc/portinfo.h
+++ libshouldbeinlibc/portinfo.h
@@ -39,8 +39,8 @@ error_t print_port_info (mach_port_t name, mach_port_type_t type, task_t task,
 			 unsigned show, FILE *stream);
 
 /* Prints info about every port in TASK that has a type in ONLY to STREAM. */
-error_t print_task_print_task_ports_info (task_t task, mach_port_type_t only,
-					  unsigned show, FILE *stream);
+error_t print_task_ports_info (task_t task, mach_port_type_t only,
+			       unsigned show, FILE *stream);
 
 /* Prints info about NAME translated through X to STREAM, in a way described
    by the flags in SHOW.  If TYPE is non-zero, it should be what
@@ -51,8 +51,8 @@ error_t print_xlated_port_info (mach_port_t name, mach_port_type_t type,
 
 /* Prints info about every port common to both tasks in X, but only if the
    port in X->from_task has a type in ONLY, to STREAM.  */
-error_t print_xlated_ports_info (struct port_name_xlator *x,
-				 mach_port_type_t only,
-				 unsigned show, FILE *stream);
+error_t print_xlated_task_ports_info (struct port_name_xlator *x,
+				      mach_port_type_t only,
+				      unsigned show, FILE *stream);
 
 #endif /* __PORTINFO_H__ */
