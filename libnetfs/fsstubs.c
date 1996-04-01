f9c7232e9992fcf73e4efa905df226f3738bddf2
Michael I. Bushnell <mib@gnu.org>
1996-04-01 21:09:34 +0000

    (netfs_S_file_exec): Remove stub.

--- libnetfs/fsstubs.c
+++ libnetfs/fsstubs.c
@@ -30,42 +30,9 @@ netfs_S_file_notice_changes (struct protid *user,
 }
 
 error_t
-netfs_S_file_get_translator (struct protid *user,
-			     char **translator,
-			     mach_msg_type_number_t *translen)
-{
-  return EOPNOTSUPP;
-}
-
-error_t
 netfs_S_file_get_translator_cntl (struct protid *user,
 				  mach_port_t *trans,
 				  mach_msg_type_name_t *transtype)
-{
-  return EOPNOTSUPP;
-}
-
-error_t 
-netfs_S_file_set_translator (struct protid *user,
-			     int pflags, int aflags,
-			     int gflags, char *passive,
-			     mach_msg_type_number_t passivelen,
-			     mach_port_t active)
-{
-  return EOPNOTSUPP;
-}
-
-error_t
-netfs_S_file_exec (struct protid *user,
-		   task_t task,
-		   int flags,
-		   char *argv, mach_msg_type_number_t argvlen,
-		   char *envp, mach_msg_type_number_t envplen,
-		   mach_port_t *fds, mach_msg_type_number_t nfds,
-		   mach_port_t *ports, mach_msg_type_number_t nports,
-		   int *ints, mach_msg_type_number_t nints,
-		   mach_port_t *dealports, mach_msg_type_number_t ndealports,
-		   mach_port_t *destports, mach_msg_type_number_t ndestports)
 {
   return EOPNOTSUPP;
 }
