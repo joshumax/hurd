0816853fa183d6c2ef7be7eed865f8e1b0fde643
Roland McGrath <roland@gnu.org>
1996-04-30 17:33:48 +0000

    Rewritten.

--- auth/auth_mig.h
+++ auth/auth_mig.h
@@ -1,24 +1,5 @@
-/* Definitions for inclusion in MiG files
-   Copyright (C) 1991, 1993, 1994 Free Software Foundation
+typedef struct authhandle *authhandle_t;
 
-This file is part of the GNU Hurd.
+authhandle_t auth_port_to_handle (auth_t);
 
-The GNU Hurd is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2, or (at your option)
-any later version.
-
-The GNU Hurd is distributed in the hope that it will be useful, 
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with the GNU Hurd; see the file COPYING.  If not, write to
-the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
-
-/* Written by Michael I. Bushnell.  */
-
-struct apt *convert_auth_to_apt (auth_t);
-typedef struct apt *aptptr_t;
-
+#include <hurd/ports.h>
