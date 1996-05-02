90048bf98c384f295979e01f8c706dc0bc3540c3
Miles Bader <miles@gnu.org>
1996-05-02 05:55:53 +0000

    Initial revision

---
+++ libshouldbeinlibc/argp-pv.c
@@ -0,0 +1,27 @@
+/* Default definition for ARGP_PROGRAM_VERSION
+
+   Copyright (C) 1996 Free Software Foundation, Inc.
+
+   Written by Miles Bader <miles@gnu.ai.mit.edu>
+
+   This file is part of the GNU Hurd.
+
+   The GNU Hurd is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   The GNU Hurd is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+
+/* If set by the user to a non-zero value, then a default option --version is
+   added (unless the ARGP_NO_HELP flag is used), which will print this string
+   and exit (unless the ARGP_NO_EXIT flag is used).  Overridden by
+   ARGP_PROGRAM_VERSION_HOOK.  */
+char *argp_program_version = 0;
