9ef23a8e7d59c1cf094aefd1d19f7ae4179a37b9
Michael I. Bushnell <mib@gnu.org>
1996-05-09 15:20:00 +0000

    (fshelp_exec_reauth): Provide new third arg to proc_setowner.

--- libfshelp/exec-reauth.c
+++ libfshelp/exec-reauth.c
@@ -1,6 +1,6 @@
 /* Setuid reauthentication for exec
 
-   Copyright (C) 1995 Free Software Foundation, Inc.
+   Copyright (C) 1995, 1996 Free Software Foundation, Inc.
 
    Written by Miles Bader <miles@gnu.ai.mit.edu>,
      from the original by Michael I. Bushnell p/BSG  <mib@gnu.ai.mit.edu>
@@ -131,7 +131,7 @@ fshelp_exec_reauth (int suid, uid_t uid, int sgid, gid_t gid,
       exec_reauth (newauth, _secure, 0, ports, num_ports, fds, num_fds);
 
       if (eff_uids->num > 0)
-	proc_setowner (ports[INIT_PORT_PROC], eff_uids->ids[0]);
+	proc_setowner (ports[INIT_PORT_PROC], eff_uids->ids[0], 0);
 
     abandon_suid:
       if (eff_uids)
