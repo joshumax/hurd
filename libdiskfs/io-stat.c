f91d82d99e98c2a6edd0d67e46ff4c024d0497ed
Michael I. Bushnell <mib@gnu.org>
1996-05-09 18:03:37 +0000

    (diskfs_S_io_stat): s/ioserver/iohelp/g.

--- libdiskfs/io-stat.c
+++ libdiskfs/io-stat.c
@@ -1,5 +1,5 @@
 /* 
-   Copyright (C) 1994, 1995 Free Software Foundation
+   Copyright (C) 1994, 1995, 1996 Free Software Foundation
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License as
@@ -31,7 +31,7 @@ diskfs_S_io_stat (struct protid *cred,
   
   np = cred->po->np;
   mutex_lock (&np->lock);
-  ioserver_get_conch (&np->conch);
+  iohelp_get_conch (&np->conch);
   if (diskfs_synchronous)
     diskfs_node_update (np, 1);
   else
