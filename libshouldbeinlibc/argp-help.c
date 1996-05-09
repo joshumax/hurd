2355899ee19b49156e8b829b1968551616dc9aae
Michael I. Bushnell <mib@gnu.org>
1996-05-09 15:04:06 +0000

    (argp_args_usage): add_usage_item -> fprintf. (argp_help): Don't shadow arg; change parm STREAM to be STREAMARG and adjust initialization of STREAM variable to use the renamed parm.

--- libshouldbeinlibc/argp-help.c
+++ libshouldbeinlibc/argp-help.c
@@ -686,7 +686,7 @@ argp_args_usage (const struct argp *argp, FILE *stream)
   const struct argp **children = argp->children;
   const char *doc = argp->args_doc;
   if (doc)
-    add_usage_item (stream, "%s", doc);
+    fprintf (stream, "%s", doc);
   if (children)
     while (*children)
       argp_args_usage (*children++, stream);
@@ -713,11 +713,11 @@ argp_doc (const struct argp *argp, FILE *stream)
 
 /* Output a usage message for ARGP to STREAM.  FLAGS are from the set
    ARGP_HELP_*.  */
-void argp_help (const struct argp *argp, FILE *stream, unsigned flags)
+void argp_help (const struct argp *argp, FILE *streamarg, unsigned flags)
 {
   int first = 1;
   struct hol *hol = 0;
-  FILE *stream = line_wrap_stream (stream, 0, RMARGIN, 0);
+  FILE *stream = line_wrap_stream (streamarg, 0, RMARGIN, 0);
 
   assert (stream);
 
