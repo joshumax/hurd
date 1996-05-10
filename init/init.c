caff2198ecfb66372a69c0cd30dfe39eb64f690b
Michael I. Bushnell <mib@gnu.org>
1996-05-10 13:25:51 +0000

    <hurd/startup_notify.h> -> "startup_notify_U.h".

--- init/init.c
+++ init/init.c
@@ -39,8 +39,8 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #include <sys/wait.h>
 #include <hurd/msg_server.h>
 #include <wire.h>
-#include <hurd/startup_notify.h>
 
+#include "startup_notify_U.h"
 #include "startup_reply_U.h"
 #include "startup_S.h"
 #include "notify_S.h"
