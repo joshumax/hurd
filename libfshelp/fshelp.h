af7907756eb963d0a051e2d53f32e1925bb2bfa4
Michael I. Bushnell <mib@gnu.org>
1996-07-03 15:29:43 +0000

    (fshelp_get_identity): New declaration.

--- libfshelp/fshelp.h
+++ libfshelp/fshelp.h
@@ -179,6 +179,21 @@ error_t fshelp_acquire_lock (struct lock_box *box, int *user,
 /* Initialize lock_box BOX.  (The user int passed to fshelp_acquire_lock
    should be initialized with LOCK_UN.).  */
 void fshelp_lock_init (struct lock_box *box);
+
+
+
+/* Return an identity port for the node numbered FILENO, suitable for
+   returning from io_identity; exactly one send right must be created
+   from the returned value.  FILENO should be the same value returned
+   as the `fileno' out-parameter in io_identity, and in the enclosing
+   directory (except for mount points), and in the st_ino stat field.
+   BUCKET should be a ports port bucket; fshelp requires the caller to
+   make sure port operations (for no-senders notifications) are used.
+   */
+mach_port_t fshelp_get_identity (struct port_bucket *bucket,
+				 ino_t fileno);
+
+
 
 /* Try to hand off responsibility from a translator to the server located on
    the node SERVER_NAME.  REQUESTOR is the translator's bootstrap port, and
