78712e7ce5b0216bc986c304f44120d4d0fa5093
Michael I. Bushnell <mib@gnu.org>
1996-07-03 15:55:32 +0000

    (netfs_make_node): Don't initialize NP->identity.

--- libnetfs/make-node.c
+++ libnetfs/make-node.c
@@ -31,7 +31,6 @@ netfs_make_node (struct netnode *nn)
   mutex_init (&np->lock);
   np->references = 1;
   np->sockaddr = MACH_PORT_NULL;
-  np->identity = MACH_PORT_NULL;
   np->owner = 0;
 
   fshelp_transbox_init (&np->transbox, &np->lock, np);
