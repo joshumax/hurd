5879f4c0c8d1c39454263b361ed1393ddad6bfa9
Miles Bader <miles@gnu.org>
1996-07-06 21:11:53 +0000

    Initial revision

---
+++ libdiskfs/opts-version.c
@@ -0,0 +1,40 @@
+/* Default hook for argp --version handling
+
+   Copyright (C) 1996 Free Software Foundation, Inc.
+
+   Written by Miles Bader <miles@gnu.ai.mit.edu>
+
+   This file is part of the GNU Hurd.
+
+   The GNU Hurd is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   The GNU Hurd is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+
+#include "priv.h"
+
+static error_t
+_print_version (FILE *stream, struct argp_state *state)
+{
+  if (argp_program_version)
+    /* If this is non-zero, then the program's probably defined it, so let
+       that take precedence over the default.  */
+    fprintf (stream, "%s\n", argp_program_version);
+  else
+    /* Construct a version using the standard diskfs variables.  */
+    fprintf (stream, "%s %d.%d\n",
+	     diskfs_server_name, diskfs_major_version, diskfs_minor_version);
+  return 0;
+}
+
+void (*argp_program_version_hook) (FILE *stream, struct argp_state *state)
+     = _print_version;
