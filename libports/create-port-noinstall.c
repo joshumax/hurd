0b43fab700f4b6e057ff9bd9e1f879575623ccfd
Miles Bader <miles@gnu.org>
1996-07-19 05:12:44 +0000

    Create a new ports port without adding it to the bucket's port-set.

---
+++ libports/create-port-noinstall.c
@@ -0,0 +1,35 @@
+/* 
+   Copyright (C) 1996 Free Software Foundation, Inc.
+   Written by Michael I. Bushnell, p/BSG.
+
+   This file is part of the GNU Hurd.
+
+   The GNU Hurd is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   The GNU Hurd is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA. */
+
+#include "ports.h"
+
+/* Create and return in RESULT a new port in CLASS and BUCKET; SIZE bytes
+   will be allocated to hold the port structure and whatever private data the
+   user desires.  */
+error_t
+ports_create_port_noinstall (struct port_class *class, 
+			     struct port_bucket *bucket,
+			     size_t size, void *result)
+{
+  return _ports_create_port_internal (class, bucket, size, result, 0);
+}
+
+
+
