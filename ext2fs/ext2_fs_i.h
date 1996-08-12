347b8d4526fc126d3af5eee4218ce565f32f3528
Miles Bader <miles@gnu.org>
1996-08-12 20:43:50 +0000

    (u32, u16, u8, s32, s16, s8):   All uses of these types changed to have a leading `__'.

--- ext2fs/ext2_fs_i.h
+++ ext2fs/ext2_fs_i.h
@@ -20,21 +20,21 @@
  * second extended file system inode data in memory
  */
 struct ext2_inode_info {
-	u32	i_data[15];
-	u32	i_flags;
-	u32	i_faddr;
-	u8	i_frag_no;
-	u8	i_frag_size;
-	u16	i_osync;
-	u32	i_file_acl;
-	u32	i_dir_acl;
-	u32	i_dtime;
-	u32	i_version;
-	u32	i_block_group;
-	u32	i_next_alloc_block;
-	u32	i_next_alloc_goal;
-	u32	i_prealloc_block;
-	u32	i_prealloc_count;
+	__u32	i_data[15];
+	__u32	i_flags;
+	__u32	i_faddr;
+	__u8	i_frag_no;
+	__u8	i_frag_size;
+	__u16	i_osync;
+	__u32	i_file_acl;
+	__u32	i_dir_acl;
+	__u32	i_dtime;
+	__u32	i_version;
+	__u32	i_block_group;
+	__u32	i_next_alloc_block;
+	__u32	i_next_alloc_goal;
+	__u32	i_prealloc_block;
+	__u32	i_prealloc_count;
 };
 
 #endif	/* _LINUX_EXT2_FS_I */
