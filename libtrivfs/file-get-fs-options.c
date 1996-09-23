46265b33f76acca44cdc0e2c312cf696ea8e0d78
Miles Bader <miles@gnu.org>
1996-09-23 23:17:08 +0000

    <argz.h>:   New include.

--- libtrivfs/file-get-fs-options.c
+++ libtrivfs/file-get-fs-options.c
@@ -18,6 +18,7 @@
    along with the GNU Hurd; see the file COPYING.  If not, write to
    the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
+#include <argz.h>
 #include <hurd/fshelp.h>
 
 #include "priv.h"
