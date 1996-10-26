5717a89bc7af7ccbd2f104551876291338f7888c
Thomas Bushnell <thomas@gnu.org>
1996-10-26 01:08:00 +0000

    Still under construction.

---
+++ defpager/backing.c
@@ -0,0 +1,131 @@
+/* Backing store management for GNU Hurd.
+   Copyright (C) 1996 Free Software Foundation, Inc.
+   Written by Thomas Bushnell, n/BSG.
+
+   This file is part of the GNU Hurd.
+
+   The GNU Hurd is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   The GNU Hurd is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+
+
+#include <hurd/store.h>
+
+const struct store_class *const permitted_classes[] = 
+{
+  &store_device_class, &store_ileave_class, &store_concat_class, 0
+};
+
+/* Allocation map, by PAGE.  */  
+/* If a bit is SET the corresponding PAGE is free. */
+char *bmap;
+
+/* Number of bytes in bmap */
+size_t bmap_len;
+
+/* Allocation rotor */
+char *bmap_rotor;
+
+struct mutex bmap_lock;
+
+error_t
+init_backing (char *name)
+{
+  error_t err;
+  int i;
+
+  err = store_open (name, STORE_NO_FILEIO, &permitted_classes, &backing_store);
+  if (err)
+    return err;
+  
+  bmap_len = backing_store->size / vm_page_size / NBBY;
+  bmap = malloc (bmap_len);
+  for (i = 0; i < bmap_len; i++)
+    bmap[i] = 0xff;
+  bmap_rotor = bmap;
+
+  /* Mark the very first page as occupied.  This makes sure we never
+     return zero offsets from allocate_backing_page (which
+     conventionally means that there is no space left.  It also makes
+     sure we don't tromp on the misfeature in Linux of using the first
+     page for permanent data. */
+  *bmap_rotor |= 1;
+}
+
+int
+allocate_backing_page ()
+{
+  int wrapped;
+  int bit;
+  int pfn;
+
+  mutex_lock (&bmap_lock);
+
+  wrapped = (bmap_rotor == bmap);
+
+  while (!wrapped || bmap_rotor < bmap + bmap_len)
+    {
+      if (bmap[bmap_rotor])
+	break;
+      bmap_rotor++;
+      if (bmap_rotor >= bmap + bmap_len)
+	wrapped++;
+    }
+  
+  if (wrapped == 2)
+    {
+      /* Didn't find one... */
+      mutex_unlock (&bmap_lock);
+      printf ("WARNING: Out of paging space; pageout failing.");
+      return 0;
+    }
+  
+  /* Find which bit */
+  bit = ffs (*bmap_rotor);
+  assert (bit);
+  bit--;
+  
+  /* Mark it */
+  *bmap_rotor |= 1 << bit;
+  
+  /* Return the correct offset */
+  pfn = (bmap_rotor - bmap) * 8 + bit;
+
+  mutex_unlock (&bmap_lock);
+  
+  return pfn * (vm_page_size / store->block_size);
+}
+
+
+void
+return_backing_pages (off_t *map, int maplen)
+{
+  int i;
+  
+  mutex_lock (&bmap_lock);
+  for (i = 0; i < maplen; i++)
+    {
+      int pfn;
+      char *b;
+      int bit;
+
+      pfn = map[i] / (vm_page_size / store->block_size);
+      b = bmap + pfn & ~7;
+      bit = pfn & 7;
+      
+      assert ((*b & (1 << bit)) == 0);
+      *b |= 1 << bit;
+    }
+  mutex_unlock (&bmap_lock);
+}
+
--- defpager/defpager.c
+++ defpager/defpager.c
@@ -1,6 +1,6 @@
 /* Default pager for GNU Hurd.
-   Copyright (C) 1994 Free Software Foundation, Inc.
-   Written by Michael I. Bushnell.
+   Copyright (C) 1996 Free Software Foundation, Inc.
+   Written by Thomas Bushnell, n/BSG.
 
    This file is part of the GNU Hurd.
 
@@ -18,30 +18,121 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
 
-/* 
- * Mach Operating System
- * Copyright (c) 1993-1989 Carnegie Mellon University
- * All Rights Reserved.
- * 
- * Permission to use, copy, modify and distribute this software and its
- * documentation is hereby granted, provided that both the copyright
- * notice and this permission notice appear in all copies of the
- * software, derivative works or modified versions, and any portions
- * thereof, and that both notices appear in supporting documentation.
- * 
- * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
- * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR
- * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
- * 
- * Carnegie Mellon requests users of this software to return to
- * 
- *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
- *  School of Computer Science
- *  Carnegie Mellon University
- *  Pittsburgh PA 15213-3890
- * 
- * any improvements or extensions that they make and grant Carnegie Mellon
- * the rights to redistribute these changes.
- */
+
+#include <hurd/pager.h>
+#include <hurd/store.h>
+
+struct user_pager_info 
+{
+  /* Size of the object */
+  vm_size_t size;
+
+  /* One entry for each page of the object. */
+  off_t *map;
+};
 
 
+/* Expand the P->map as necessary to handle an incoming request of the
+   page at ADDR. */
+static inline void
+expand_map (struct user_pager_info *p, vm_offset_t addr)
+{
+  /* See if this is beyond the current extent */
+  if (page >= pager->size)
+    {
+      off_t *newmap;
+      vm_size_t newsize;
+
+      newsize = page + vm_page_size;
+      newmap = realloc (pager->map, size / vm_page_size * sizeof (off_t));
+
+      bzero (pager->map + pager->size / vm_page_size * sizeof (off_t),
+	     (newsize - pager->size) / vm_page_size * sizeof (off_t));
+      pager->size = newsize;
+      pager->map = newmap;
+    }
+}
+
+
+error_t
+pager_read_page (struct user_pager_info *pager,
+		 vm_offset_t page,
+		 vm_address_t *buf,
+		 int *write_lock)
+{
+  int pfn = page / vm_page_size;
+  size_t nread;
+  
+  /* We never request write locks. */
+  *write_lock = 0;
+
+  expand_map (pager, page);
+
+  if (!pager->map[pfn])
+    vm_allocate (mach_task_self (), buf, vm_page_size, 1);
+  else
+    {
+      store_read (backing_store, pager->map[pfn], vm_page_size, 
+		  (void **)buf, &nread);
+      if (nread != vm_page_size)
+	{
+	  vm_deallocate (mach_task_self (), *buf, nread);
+	  return EIO;
+	}
+    }
+  return 0;
+}
+
+      
+error_t
+pager_write_page (struct user_pager_info *pager,
+		  vm_offset_t page,
+		  vm_address_t buf)
+{
+  int pfn = page / vm_page_size;
+  size_t nwritten;
+  
+  expand_map (pager, page);
+  
+  if (!pager->map[pfn])
+    pager->map[pfn] = allocate_backing_page ();
+  
+  /* No more backing store.  Oh dear. */
+  if (!pager->map[pfn])
+    return EIO;
+  
+  err = store_write (backing_store, pager->map[pfn], (void *) buf, 
+		     vm_page_size, &nwritten);
+  if (!err && nwritten != vm_page_size)
+    err = EIO;
+  return err;
+}
+
+error_t
+pager_unlock_page (struct user_pager_info *pager,
+		   vm_offset_t address)
+{
+  return 0;
+}
+
+error_t
+pager_report_extent (struct user_pager_info *pager,
+		     vm_address_t *offset,
+		     vm_size_t *size)
+{
+  *offset = 0;
+  *size = pager->size;
+  return 0;
+}
+
+void
+pager_clear_user_data (struct user_pager_info *pager)
+{
+  return_backing_pages (pager->map, pager->size / vm_page_size);
+  free (pager->map);
+}
+
+void
+pager_dropweak (struct user_pager_info *pager)
+{
+}
