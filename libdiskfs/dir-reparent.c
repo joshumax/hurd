601e7e024b01a0fb75f7b2e1aaaac765976884c9
Miles Bader <miles@gnu.org>
1997-02-05 16:37:51 +0000

    Initial checkin.

---
+++ libdiskfs/dir-reparent.c
@@ -0,0 +1,29 @@
+/* Reparent a directory
+
+   Copyright (C) 1997 Free Software Foundation
+
+   This file is part of the GNU Hurd.
+
+   The GNU Hurd is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   The GNU Hurd is distributed in the hope that it will be useful, 
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with the GNU Hurd; see the file COPYING.  If not, write to
+   the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+#include "priv.h"
+#include "fs_S.h"
+
+error_t
+diskfs_S_dir_reparent (struct protid *cred, mach_port_t parent,
+		       mach_port_t *new_dir, mach_msg_type_name_t *new_dir_type)
+{
+  return EOPNOTSUPP;
+}
