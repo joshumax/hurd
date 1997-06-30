0b1d700aff043863b8863d227409f8dde39e6050
Miles Bader <miles@gnu.org>
1997-06-30 19:00:30 +0000

    (main):   Arg parsing added. (argp_program_version):   New variable. Include <argp.h>.

--- exec/main.c
+++ exec/main.c
@@ -1,28 +1,31 @@
 /* GNU Hurd standard exec server, main program and server mechanics.
-   Copyright (C) 1992, 1993, 19941996, 1996 Free Software Foundation, Inc.
+
+   Copyright (C) 1992, 1993, 1994, 1995, 1996, 1997 Free Software Foundation, Inc.
    Written by Roland McGrath.
+   This file is part of the GNU Hurd.
 
-This file is part of the GNU Hurd.
+   The GNU Hurd is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
 
-The GNU Hurd is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2, or (at your option)
-any later version.
+   The GNU Hurd is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-The GNU Hurd is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with the GNU Hurd; see the file COPYING.  If not, write to
-the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+   You should have received a copy of the GNU General Public License
+   along with the GNU Hurd; see the file COPYING.  If not, write to
+   the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #include "priv.h"
 #include <error.h>
 #include <hurd/paths.h>
 #include <hurd/startup.h>
+#include <argp.h>
 #include <version.h>
+
+const char *argp_program_version = STANDARD_HURD_VERSION (proc);
 
 #ifdef BFD
 bfd_arch_info_type host_bfd_arch_info;
@@ -113,6 +116,9 @@ main (int argc, char **argv)
 {
   error_t err;
   mach_port_t bootstrap;
+  struct argp argp = { 0, 0, 0, "Hurd standard exec server" };
+
+  argp_parse (&argp, argc, argv, 0, 0, 0);
 
   save_argv = argv;
 
