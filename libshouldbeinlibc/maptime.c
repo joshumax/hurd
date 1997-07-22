cd9ac4936e70c95561e9c4c8044d427ca61ecbc9
Miles Bader <miles@gnu.org>
1997-07-22 19:20:30 +0000

    (maptime_map):   For the device-file case, don't use wierd file_get_storage_info method for     getting device port, just use io_map to get a memory object directly from     the file.

--- libshouldbeinlibc/maptime.c
+++ libshouldbeinlibc/maptime.c
@@ -1,6 +1,6 @@
 /* Support for mach's mapped time
 
-   Copyright (C) 1996 Free Software Foundation, Inc.
+   Copyright (C) 1996, 1997 Free Software Foundation, Inc.
 
    Written by Miles Bader <miles@gnu.ai.mit.edu>
 
@@ -18,6 +18,7 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
 
+#include <fcntl.h>
 #include <hurd.h>
 #include <mach.h>
 #include <device/device.h>
@@ -34,11 +35,11 @@ maptime_map (int use_mach_dev, char *dev_name,
 	     volatile struct mapped_time_value **mtime)
 {
   error_t err;
-  device_t device;  
-  mach_port_t mobj;
+  mach_port_t memobj;
 
   if (use_mach_dev)
     {
+      device_t device;  
       mach_port_t device_master;
 
       err = get_privileged_ports (0, &device_master);
@@ -47,72 +48,31 @@ maptime_map (int use_mach_dev, char *dev_name,
 	  err = device_open (device_master, 0, dev_name ?: "time", &device);
 	  mach_port_deallocate (mach_task_self (), device_master);
 	}
+
+      err = device_map (device, VM_PROT_READ, 0, sizeof *mtime, &memobj, 0);
     }
   else
     {
-      mach_msg_type_number_t data_len = 100;
-      mach_msg_type_number_t num_ints = 10, num_ports = 10, num_offsets = 10;
-      int _ints[num_ints], *ints = _ints;
-      mach_port_t _ports[num_ports], *ports = _ports;
-      off_t _offsets[num_offsets], *offsets = _offsets;
-      char _data[data_len], *data = _data;
-      file_t node = file_name_lookup (dev_name ?: "/dev/time", 0, 0);
+      mach_port_t wr_memobj;
+      file_t node = file_name_lookup (dev_name ?: "/dev/time", O_RDONLY, 0);
 
       if (node == MACH_PORT_NULL)
 	return errno;
 
-      err = file_get_storage_info (node, &ports, &num_ports, &ints, &num_ints,
-				   &offsets, &num_offsets, &data, &data_len);
-
-      if (! err)
-	{
-	  int i;
-
-	  if (num_ints >= 6 && ints[0] == STORAGE_DEVICE)
-	    /* This a device.  */
-	    if (num_ports != 1)
-	      err = EGRATUITOUS;
-	    else if (! MACH_PORT_VALID (ports[0]))
-	      err = EPERM;	/* Didn't pass back the device port.  XXX */
-	    else
-	      {
-		device = ports[0];
-		ports[0] = MACH_PORT_NULL; /* Don't deallocate here.  */
-	      }
-	  else
-	    err = ENODEV;	/* Not admitting to being a device.  XXX */
-
-	  /* Deallocate any ports we got back.  */
-	  for (i = 0; i < num_ports; i++)
-	    if (MACH_PORT_VALID (ports[i]))
-	      mach_port_deallocate (mach_task_self (), ports[i]);
-
-	  /* Deallocate any out of line vectors return by gsi.  */ 
-#define DISCARD_MEM(v, vl, b)						    \
-	  if (vl && v != b)						    \
-	    vm_deallocate (mach_task_self (), (vm_address_t)v, vl * sizeof *v);
-	  DISCARD_MEM (ints, num_ints, _ints);
-	  DISCARD_MEM (offsets, num_offsets, _offsets);
-	  DISCARD_MEM (ports, num_ports, _ports);
-	  DISCARD_MEM (data, data_len, _data);
-	}
+      err = io_map (node, &memobj, &wr_memobj);
+      if (!err && wr_memobj != MACH_PORT_NULL)
+	mach_port_deallocate (mach_task_self (), wr_memobj);
 
       mach_port_deallocate (mach_task_self (), node);
     }
 
-  if (err)
-    return err;
-
-  err = device_map (device, VM_PROT_READ, 0, sizeof *mtime, &mobj, 0);
   if (! err)
     {
       err =
 	vm_map (mach_task_self (), (vm_address_t *)mtime, sizeof *mtime, 0, 1,
-		mobj, 0, 0, VM_PROT_READ, VM_PROT_READ, VM_INHERIT_NONE);
-      mach_port_deallocate (mach_task_self (), mobj);
+		memobj, 0, 0, VM_PROT_READ, VM_PROT_READ, VM_INHERIT_NONE);
+      mach_port_deallocate (mach_task_self (), memobj);
     }
-
-  mach_port_deallocate (mach_task_self (), device);
 
   return err;
 }
