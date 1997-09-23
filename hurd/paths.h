2414fffb156b8ffd627719b6e216ce6a6702635c
Thomas Bushnell <thomas@gnu.org>
1997-09-23 19:57:40 +0000

    Tue Sep 23 15:52:35 1997  Thomas Bushnell, n/BSG  <thomas@gnu.ai.mit.edu>
    	* password.defs: New file.
    	* paths.h (_SERVERS_PASSWORD): New entry.
    	* subsystems: Add password at 38000.

--- hurd/paths.h
+++ hurd/paths.h
@@ -1,5 +1,5 @@
 /* Standard Hurd pathnames.
-   Copyright (C) 1992, 1994, 1995 Free Software Foundation, Inc.
+   Copyright (C) 1992, 1994, 1995, 1997 Free Software Foundation, Inc.
 
 This file is part of the GNU Hurd.
 
@@ -28,6 +28,7 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #define	_SERVERS_EXEC		_SERVERS "exec"
 #define _SERVERS_STARTUP	_SERVERS "startup"
 #define _SERVERS_PROC		_SERVERS "proc"
+#define _SERVERS_PASSWORD	_SERVERS "password"
 
 /* Directory containing naming points for socket servers.
    Entries are named by the string representing the domain number
