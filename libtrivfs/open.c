c8d2da69861cf22e740b26d81e326d1e02835d56
Thomas Bushnell <thomas@gnu.org>
1999-02-13 09:13:36 +0000

    1999-02-12  Gordon Matzigkeit  <gord@trick.fig.org>
    	* open.c (trivfs_open): Add missing `=' from last change.

--- libtrivfs/open.c
+++ libtrivfs/open.c
@@ -56,7 +56,7 @@ trivfs_open (struct trivfs_control *cntl,
       if (! err)
 	{
 	  new->user = user;
-	  new->isroot idvec_contains (user->uids, 0);
+	  new->isroot = idvec_contains (user->uids, 0);
 
 	  new->po = po;
 	  new->hook = 0;
