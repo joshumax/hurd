113320c6cc5d0b527b8d14ec9df1c6b07496bc3b
Thomas Bushnell <thomas@gnu.org>
1999-02-16 10:56:58 +0000

    Tue Feb 16 05:47:57 1999  Thomas Bushnell, BSG  <tb@mit.edu>
    	* io-revoke.c: New file.
    	* Makefile (IOSRCS): Add io-revoke.c.

---
+++ libtrivfs/io-revoke.c
@@ -0,0 +1,32 @@
+/* 
+   Copyright (C) 1999 Free Software Foundation
+   Written by Thomas Bushnell, BSG.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+
+#include "priv.h"
+#include "io_S.h"
+
+/* Implement io_revoke as described in <hurd/io.defs>. */
+kern_return_t
+trivfs_S_io_revoke (struct trivfs_protid *cred)
+{
+  /* Revoke of the underlying node is actually generally right,
+     because that will cause actual calls to fail.  In any case,
+     we don't have the ability to check permissions ourselves 
+     correctly. */
+
+  return cred ? io_revoke (cred->realnode) : EOPNOTSUPP;
+}
