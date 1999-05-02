bf281d6870546959ff35f51851a47e71423cae32
Roland McGrath <roland@gnu.org>
1999-05-02 19:53:18 +0000

    1999-05-02  Roland McGrath  <roland@baalperazim.frob.com>
    	* main.c: Include <error.h>.

--- proc/main.c
+++ proc/main.c
@@ -26,6 +26,7 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #include <assert.h>
 #include <wire.h>
 #include <argp.h>
+#include <error.h>
 #include <version.h>
 
 #include "proc.h"
