4a5464f7a32ec95ce6e2a9f488fff94d70698467
Roland McGrath <roland@gnu.org>
1999-10-03 05:05:02 +0000

    1999-10-03  Roland McGrath  <roland@baalperazim.frob.com>
    	* bitmap.c (ffz): Don't subtract one (duh).

--- ext2fs/bitmap.c
+++ ext2fs/bitmap.c
@@ -18,7 +18,7 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
 
-#define ffz(word)	(ffs (~(word)) - 1)
+#define ffz(word)	(ffs (~(word)))
 
 /*
  *  linux/fs/ext2/bitmap.c (&c)
