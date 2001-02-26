ad6dbaf6541730c0c98f509425464491bb3062a0
Roland McGrath <roland@gnu.org>
2001-02-26 04:26:44 +0000

    2001-02-25  Roland McGrath  <roland@frob.com>
    	* Makefile (asm/checksum.h): New target.
    	(CPPFLAGS): Don't set _HURD_SYSTYPE.
    	* glue-include/asm/checksum.h: File removed.

--- pfinet/glue-include/asm/checksum.h
+++
@@ -1,5 +0,0 @@
-/* This is the only file from the Linux include/asm-* directory
-   that we use, so we use this magic file here rather than making a
-   symlink asm -> .../linux-src/include/asm-SYSTYPE somewhere.  */
-
-#include "../../linux-src/include/asm-" _HURD_SYSTYPE "/checksum.h"
