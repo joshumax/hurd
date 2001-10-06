45151e7bd31478d584dec0a03092201eed5fd56d
Roland McGrath <roland@gnu.org>
2001-10-06 18:33:51 +0000

    2001-10-06  Neal H Walfield  <neal@cs.uml.edu>
            * memobj.c: Include "store.h", not <hurd/store.h>.
            * nbd.c: Likewise.
            * parted.c: Likewise.

--- libstore/nbd.c
+++ libstore/nbd.c
@@ -17,8 +17,8 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111, USA. */
 
+#include "store.h"
 #include <hurd.h>
-#include <hurd/store.h>
 #include <hurd/io.h>
 #include <errno.h>
 #include <sys/socket.h>
