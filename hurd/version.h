b5b24837bba07e623cdf35f7bb1161ca2bcbdacb
Roland McGrath <roland@gnu.org>
2002-06-11 21:37:22 +0000

    2002-06-09  Roland McGrath  <roland@frob.com>
    	* version.h: New file.
    	* Makefile (INSTHDRS): Add it.

---
+++ hurd/version.h
@@ -0,0 +1,6 @@
+/* This file just gives a value that can be tested easily with #if as a
+   gross check of the Hurd interface version.  It has the format YYYYMMDD
+   and will only ever be increased.  This will be bumped whenever either
+   the RPC interfaces or the library APIs change.  */
+
+#define HURD_INTERFACE_VERSION	20020609
