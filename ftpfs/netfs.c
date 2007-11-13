b1720099133811c82a1ab6d9cec835666459638c
Thomas Schwinge <tschwinge@gnu.org>
2007-11-13 23:06:58 +0000

    2007-11-13  Thomas Schwinge  <tschwinge@gnu.org>
    	* netfs.c (netfs_attempt_utimes): Adapt to ``struct stat'' changes.

--- ftpfs/netfs.c
+++ ftpfs/netfs.c
@@ -1,7 +1,9 @@
 /* ftpfs interface to libnetfs
 
-   Copyright (C) 1997,98,99,2001 Free Software Foundation, Inc.
+   Copyright (C) 1997, 1998, 1999, 2001, 2007 Free Software Foundation, Inc.
+
    Written by Miles Bader <miles@gnu.org>
+
    This file is part of the GNU Hurd.
 
    The GNU Hurd is free software; you can redistribute it and/or
@@ -75,18 +77,12 @@ netfs_attempt_utimes (struct iouser *cred, struct node *node,
   if (! err)
     {
       if (atime)
-	{
-	  node->nn_stat.st_atime = atime->tv_sec;
-	  node->nn_stat.st_atime_usec = atime->tv_nsec / 1000;
-	}
+	node->nn_stat.st_atim = *atime;
       else
 	flags |= TOUCH_ATIME;
 
       if (mtime)
-	{
-	  node->nn_stat.st_mtime = mtime->tv_sec;
-	  node->nn_stat.st_mtime_usec = mtime->tv_nsec / 1000;
-	}
+	node->nn_stat.st_mtim = *mtime;
       else
 	flags |= TOUCH_MTIME;
 
