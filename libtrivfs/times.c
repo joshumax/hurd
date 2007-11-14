696a7d9c2343c7e88cee73b6c517c3f6f2158ab1
Thomas Schwinge <tschwinge@gnu.org>
2007-11-14 12:16:25 +0000

    Update copyright year.

--- libtrivfs/times.c
+++ libtrivfs/times.c
@@ -1,5 +1,5 @@
 /* 
-   Copyright (C) 1994, 1999 Free Software Foundation
+   Copyright (C) 1994, 1999, 2007 Free Software Foundation
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License as
