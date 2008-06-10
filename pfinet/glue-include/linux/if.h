9c4205108f5c97e884f63aab6b69cad5fb9b793d
Samuel Thibault <samuel.thibault@ens-lyon.org>
2008-06-10 21:56:19 +0000

    * glue-include/linux/if.h (IFF_DYNAMIC): Remove macro.

--- pfinet/glue-include/linux/if.h
+++ pfinet/glue-include/linux/if.h
@@ -1,4 +1,3 @@
 #include <net/if.h>
 
 #define IFF_VOLATILE    (IFF_LOOPBACK|IFF_POINTOPOINT|IFF_BROADCAST|IFF_ALLMULTI)
-#define IFF_DYNAMIC     0x8000          /* dialup device with changing addresses*/
