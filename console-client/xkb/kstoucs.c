1afa200c3d5d8f907a67c71c03bc3fd07d838276
Samuel Thibault <samuel.thibault@ens-lyon.org>
2013-05-01 01:36:14 +0200

    Fix console hang on unknown key press
    * console-client/xkb/kstoucs.c (find_ucs): Remove middle value from
    recursive call range.

--- console-client/xkb/kstoucs.c
+++ console-client/xkb/kstoucs.c
@@ -34,9 +34,9 @@ unsigned int doit (int keysym)
       return 0; /* base case: empty search space. */
     /* recursive cases: halve search space. */
     else if (middle->keysym < keysym)
-      return find_ucs (keysym, middle, last);
+      return find_ucs (keysym, middle+1, last);
     else if (middle->keysym > keysym)
-      return find_ucs (keysym, first, middle);
+      return find_ucs (keysym, first, middle-1);
   }
 
   #define NUM_KEYSYMS (sizeof kstoucs_map / sizeof(struct ksmap))
