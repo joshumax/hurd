5e3d2fc37be8b73a0ddabf1389c75f79e77c7e93
Justus Winter <4winter@informatik.uni-hamburg.de>
2013-11-20 13:19:36 +0100

    libshouldbeinlibc: fix error handling in maptime_map
    Found using the Clang Static Analyzer.
    
    * libshouldbeinlibc/maptime.c (maptime_map): Fix error handling.

--- libshouldbeinlibc/maptime.c
+++ libshouldbeinlibc/maptime.c
@@ -42,13 +42,13 @@ maptime_map (int use_mach_dev, char *dev_name,
       mach_port_t device_master;
 
       err = get_privileged_ports (0, &device_master);
-      if (! err)
-	{
-	  err = device_open (device_master, 0, dev_name ?: "time", &device);
-	  mach_port_deallocate (mach_task_self (), device_master);
-	  if (err)
-	    return err;
-	}
+      if (err)
+	return err;
+
+      err = device_open (device_master, 0, dev_name ?: "time", &device);
+      mach_port_deallocate (mach_task_self (), device_master);
+      if (err)
+	return err;
 
       err = device_map (device, VM_PROT_READ, 0, sizeof *mtime, &memobj, 0);
     }
