80a50c59331faf268947f97ca702cb9a1d9881cd
Justus Winter <4winter@informatik.uni-hamburg.de>
2014-04-26 13:16:23 +0200

    libdiskfs: set the default sync interval to 30 seconds
    The default sync interval has been changed in 9e55fdd7 from 30 to 5
    seconds.  This change was not reflected in the documentation.
    
    At least for current hardware, using 30 seconds instead of just 5
    alleviates the thread-storm problem.  Make 30 seconds the default
    again.
    
    * libdiskfs/priv.h (DEFAULT_SYNC_INTERVAL): Set to 30 seconds.

--- libdiskfs/priv.h
+++ libdiskfs/priv.h
@@ -63,7 +63,7 @@ extern const struct argp_option diskfs_common_options[];
 #define OPT_INHERIT_DIR_GROUP		604	/* --inherit-dir-group */
 
 /* Common value for diskfs_common_options and diskfs_default_sync_interval. */
-#define DEFAULT_SYNC_INTERVAL 5
+#define DEFAULT_SYNC_INTERVAL 30
 #define DEFAULT_SYNC_INTERVAL_STRING STRINGIFY(DEFAULT_SYNC_INTERVAL)
 #define STRINGIFY(x) STRINGIFY_1(x)
 #define STRINGIFY_1(x) #x
