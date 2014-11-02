bd611d2bbd2a7faa24ce8d3b1a422cebb8f71e8b
Samuel Thibault <samuel.thibault@ens-lyon.org>
2014-11-03 00:02:52 +0100

    Use a mere weak attribute instead of a weak alias
    We don't need to call the original function, so a weak attribute is
    enough.
    
    * libports/dead-name.c (ports_dead_name): Remove weak alias.
    (__ports_dead_name): Rename back to ports_dead_name, but add weak attribute.

--- libports/dead-name.c
+++ libports/dead-name.c
@@ -21,9 +21,8 @@
 #include "ports.h"
 #include <mach/notify.h>
 
-void
-__ports_dead_name (void *notify, mach_port_t dead_name)
+void __attribute__ ((weak))
+ports_dead_name (void *notify, mach_port_t dead_name)
 {
   ports_interrupt_notified_rpcs (notify, dead_name, MACH_NOTIFY_DEAD_NAME);
 }
-weak_alias (__ports_dead_name, ports_dead_name);
