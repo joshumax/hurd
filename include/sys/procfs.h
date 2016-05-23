305e83c42624c8cf84452d5d0fa7669e2af6f997
Thomas Schwinge <thomas@codesourcery.com>
2016-05-23 11:23:57 +0200

    Make <sys/procfs.h> self-contained again
    Commit e914bfc3d6e5ddf6f8c5e93a4334873a48a24ddf made <sys/procfs.h>
    Autoconf/configure tests change as follows:
    
        checking sys/procfs.h usability... [-yes-]{+no+}
        checking sys/procfs.h presence... yes
        {+configure: WARNING: sys/procfs.h: present but cannot be compiled+}
        {+configure: WARNING: sys/procfs.h:     check for missing prerequisite headers?+}
        {+configure: WARNING: sys/procfs.h: see the Autoconf documentation+}
        {+configure: WARNING: sys/procfs.h:     section "Present But Cannot Be Compiled"+}
        {+configure: WARNING: sys/procfs.h: proceeding with the compiler's result+}
        checking for sys/procfs.h...[-yes-]{+no+}
        [-checking for prstatus_t in sys/procfs.h... no-]
        [-checking for prstatus32_t in sys/procfs.h... no-]
        [-checking for prstatus_t.pr_who in sys/procfs.h... no-]
        [-checking for prstatus32_t.pr_who in sys/procfs.h... no-]
        [-checking for pstatus_t in sys/procfs.h... yes-]
        [-checking for pxstatus_t in sys/procfs.h... no-]
        [-checking for pstatus32_t in sys/procfs.h... no-]
        [-checking for prpsinfo_t in sys/procfs.h... no-]
        [-checking for prpsinfo_t.pr_pid in sys/procfs.h... no-]
        [-checking for prpsinfo32_t in sys/procfs.h... no-]
        [-checking for prpsinfo32_t.pr_pid in sys/procfs.h... no-]
        [-checking for psinfo_t in sys/procfs.h... yes-]
        [-checking for psinfo_t.pr_pid in sys/procfs.h... yes-]
        [-checking for psinfo32_t in sys/procfs.h... no-]
        [-checking for psinfo32_t.pr_pid in sys/procfs.h... no-]
        [-checking for lwpstatus_t in sys/procfs.h... yes-]
        [-checking for lwpxstatus_t in sys/procfs.h... no-]
        [-checking for lwpstatus_t.pr_context in sys/procfs.h... no-]
        [-checking for lwpstatus_t.pr_reg in sys/procfs.h... yes-]
        [-checking for lwpstatus_t.pr_fpreg in sys/procfs.h... yes-]
        [-checking for win32_pstatus_t in sys/procfs.h... no-]
    
    That is because of:
    
        $ echo '#include <sys/procfs.h>' | gcc -x c - -o /dev/null -S
        In file included from <stdin>:1:0:
        /usr/include/sys/procfs.h:66:3: error: unknown type name ‘vm_address_t’
           vm_address_t pr_argv;  /* Original argument vector address.  */
           ^
        /usr/include/sys/procfs.h:67:3: error: unknown type name ‘vm_address_t’
           vm_address_t pr_envp;  /* Original environment vector address.  */
           ^
    
    * include/sys/procfs.h: Include <mach/std_types.h> to make file self-contained
    again.

--- include/sys/procfs.h
+++ include/sys/procfs.h
@@ -1,5 +1,5 @@
 /* <sys/procfs.h> -- data structures describing ELF core file formats
-   Copyright (C) 2002 Free Software Foundation, Inc.
+   Copyright (C) 2002, 2015, 2016 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -29,6 +29,7 @@
 
 #include <features.h>
 #include <inttypes.h>
+#include <mach/std_types.h>
 #include <sys/types.h>
 #include <sys/time.h>
 #include <ucontext.h>
