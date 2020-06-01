9e3b662812cebe8113a144c705eddbbd3855f54b
Samuel Thibault <samuel.thibault@ens-lyon.org>
2020-06-01 18:35:28 +0200

    libdiskfs: Drop spurious mutex acquisition
    diskfs_make_peropen does not need node to be locked. Worse, it could try to
    lock it.
    
    * libdiskfs/file-reparent.c (diskfs_S_file_reparent): Do not lock node
    around diskfs_make_peropen.

--- libdiskfs/file-reparent.c
+++ libdiskfs/file-reparent.c
@@ -37,7 +37,6 @@ diskfs_S_file_reparent (struct protid *cred, mach_port_t parent,
   
   node = cred->po->np;
 
-  pthread_mutex_lock (&node->lock);
   err = diskfs_make_peropen (node, cred->po->openstat, cred->po, &new_po);
   if (! err)
     {
@@ -45,7 +44,6 @@ diskfs_S_file_reparent (struct protid *cred, mach_port_t parent,
       if (err)
 	diskfs_release_peropen (new_po);
     }
-  pthread_mutex_unlock (&node->lock);
 
   if (! err)
     {
