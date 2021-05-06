a9f1e487350cca14ac7df732ac24437822d4eac9
Sergey Bugaev <bugaevc@gmail.com>
2021-05-06 15:56:26 +0300

    libpager: Fix mixing up success and error
    _pager_pagemap_resize () returns an error or 0 on success,
    not a boolean.
    Message-Id: <20210506125631.79117-2-bugaevc@gmail.com>

--- libpager/offer-page.c
+++ libpager/offer-page.c
@@ -31,21 +31,22 @@ pager_offer_page (struct pager *p,
   pthread_mutex_lock (&p->interlock);
 
   if (_pager_pagemap_resize (p, offset + vm_page_size))
+    goto release_out;
+
+  short *pm_entry = &p->pagemap[offset / vm_page_size];
+
+  while (*pm_entry & PM_INCORE)
     {
-      short *pm_entry = &p->pagemap[offset / vm_page_size];
-
-      while (*pm_entry & PM_INCORE)
-	{
-	  pthread_mutex_unlock (&p->interlock);
-	  pager_flush_some (p, offset, vm_page_size, 1);
-	  pthread_mutex_lock (&p->interlock);
-	}
-      *pm_entry |= PM_INCORE;
-
-      memory_object_data_supply (p->memobjcntl, offset, buf, vm_page_size, 0,
-				 writelock ? VM_PROT_WRITE : VM_PROT_NONE, 
-				 precious, MACH_PORT_NULL);
+      pthread_mutex_unlock (&p->interlock);
+      pager_flush_some (p, offset, vm_page_size, 1);
+      pthread_mutex_lock (&p->interlock);
     }
+  *pm_entry |= PM_INCORE;
 
+  memory_object_data_supply (p->memobjcntl, offset, buf, vm_page_size, 0,
+                             writelock ? VM_PROT_WRITE : VM_PROT_NONE,
+                             precious, MACH_PORT_NULL);
+
+ release_out:
   pthread_mutex_unlock (&p->interlock);
 }
