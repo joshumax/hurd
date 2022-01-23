f40c6335ba84c05417e61696eb8ef1ea2ad5d75f
Etienne Brateau <etienne.brateau@gmail.com>
2022-01-23 05:17:10 +0100

    ext2fs: detect and warn when filesystem is an ext3 fs
    Message-Id: <20220123041715.19402-12-etienne.brateau@gmail.com>

--- ext2fs/hyper.c
+++ ext2fs/hyper.c
@@ -134,6 +134,8 @@ get_hypermetadata (void)
 	}
       if (le16toh (sblock->s_inode_size) != EXT2_GOOD_OLD_INODE_SIZE)
 	ext2_panic ("inode size %d isn't supported", le16toh (sblock->s_inode_size));
+      if (EXT2_HAS_COMPAT_FEATURE (sblock, EXT3_FEATURE_COMPAT_HAS_JOURNAL))
+        ext2_warning ("mounting ext3 filesystem as ext2");
     }
 
   groups_count =
