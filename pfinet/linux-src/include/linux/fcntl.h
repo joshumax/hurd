ce0fbff2a9d7daba2c97a33af9141efe7c7824a6
Flavio Cruz <flaviocruz@gmail.com>
2022-11-03 01:09:21 -0400

    Remove duplicate pfinet linux headers that have glue versions
    Message-Id: <Y2NNAegVOKg/X7OC@viriathus>

--- pfinet/linux-src/include/linux/fcntl.h
+++
@@ -1,6 +0,0 @@
-#ifndef _LINUX_FCNTL_H
-#define _LINUX_FCNTL_H
-
-#include <asm/fcntl.h>
-
-#endif
--- pfinet/linux-src/include/linux/malloc.h
+++
@@ -1,5 +0,0 @@
-#ifndef _LINUX_MALLOC_H
-#define _LINUX_MALLOC_H
-
-#include <linux/slab.h>
-#endif /* _LINUX_MALLOC_H */
