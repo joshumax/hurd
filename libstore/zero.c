6f3d68bc8b46bdc7b0f5f43918744d99746672a2
Samuel Thibault <samuel.thibault@ens-lyon.org>
2023-02-27 09:38:22 +0100

    libstore: Fix zero store size computation
    56d065041793 ("libstore: Fix undefined behavior") missed letting the sign
    bit be 0, thus leading to a negative store size, and thus /dev/zero
    would reject any read/write.

--- libstore/zero.c
+++ libstore/zero.c
@@ -150,8 +150,9 @@ zero_open (const char *name, int flags,
     }
   else
     {
-      store_offset_t max_offs = ~((store_offset_t)1
-				  << (CHAR_BIT * sizeof (store_offset_t) - 2));
+      store_offset_t max_offs = (
+	  ((store_offset_t)1 << (CHAR_BIT * sizeof (store_offset_t) - 2))
+	  - 1) * 2 + 1;
       return store_zero_create (max_offs, flags, store);
     }
 }
