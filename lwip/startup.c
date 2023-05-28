1d289b9b38f7da8c2859491f91dacb717cf82501
Samuel Thibault <samuel.thibault@ens-lyon.org>
2023-05-28 16:33:27 +0200

    lwip: Fix port leak on /servers/startup lookup error

--- lwip/startup.c
+++ lwip/startup.c
@@ -58,7 +58,10 @@ arrange_shutdown_notification (void)
 
   initport = file_name_lookup (_SERVERS_STARTUP, 0, 0);
   if (initport == MACH_PORT_NULL)
-    return;
+    {
+      ports_port_deref (pi);
+      return;
+    }
 
   notify = ports_get_send_right (pi);
   ports_port_deref (pi);
