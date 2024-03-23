ddc4f0144a709cebc4dae321b810e85547856b09
Sergey Bugaev <bugaevc@gmail.com>
2024-03-23 14:53:16 +0300

    proc: Only try host_kernel_version () on i386
    None of the new architectures are going to have it; that isn't specific
    to x86_64.
    Message-ID: <20240323115322.69075-4-bugaevc@gmail.com>

--- proc/host.c
+++ proc/host.c
@@ -371,8 +371,8 @@ initialize_version_info (void)
   server_versions_nalloc = 10;
 
   err = host_get_kernel_version (mach_host_self (), kv);
-#ifndef __x86_64__
-  /* We don't support host_kernel_version for x86_64. */
+#ifdef __i386__
+  /* We only supported host_kernel_version on i386.  */
   if (err == MIG_BAD_ID)
     {
       /* Delete after some time. */
