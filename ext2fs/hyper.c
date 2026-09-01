982d0003d9b0467944aeed56c6661b8fb84a9277
Milos Nikic <nikic.milos@gmail.com>
2026-09-01 23:50:12 +0200

    ext2fs: Fix indent

--- ext2fs/hyper.c
+++ ext2fs/hyper.c
@@ -216,18 +216,18 @@ diskfs_set_hypermetadata (int wait, int clean)
       wait = 1;
     }
 
- if (sblock_dirty)
-   {
-     if (diskfs_readonly)
-       return EROFS; /* impossible to write */
+  if (sblock_dirty)
+    {
+      if (diskfs_readonly)
+        return EROFS; /* impossible to write */
 
-     /* Before writing, set the time of write */
-     sblock->s_wtime = htole32 (diskfs_mtime->seconds);
-     sblock_dirty = 0;
-     memcpy (mapped_sblock, sblock, SBLOCK_SIZE);
-     disk_cache_block_ref_ptr (mapped_sblock);
-     record_global_poke (mapped_sblock);
-   }
+      /* Before writing, set the time of write */
+      sblock->s_wtime = htole32 (diskfs_mtime->seconds);
+      sblock_dirty = 0;
+      memcpy (mapped_sblock, sblock, SBLOCK_SIZE);
+      disk_cache_block_ref_ptr (mapped_sblock);
+      record_global_poke (mapped_sblock);
+    }
 
   sync_global (wait);
   if (wait)
